67f2d3a450767af1737427d8c5ad63174a417eaa
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-06-24 14:21:15 -0400

    reorder file

--- rf_pipelines_c.cpp
+++ rf_pipelines_c.cpp
@@ -6,147 +6,7 @@ using namespace std;
 
 // -------------------------------------------------------------------------------------------------
 //
-// wi_stream wrapper class
-
-
-struct wi_stream_object {
-    PyObject_HEAD
-
-    // "Borrowed" reference (delete will not be called in tp_dealloc())
-    rf_pipelines::wi_stream *pbare;
-
-    // Reference held through shared_ptr.  Using a pointer-to-a-shared pointer was least awkward here.
-    shared_ptr<rf_pipelines::wi_stream> *pshared;
-
-    // note: no tp_alloc function is necessary, since the default (PyType_GenericAlloc()) calls memset().    
-    static void tp_dealloc(PyObject *self_)
-    {
-	wi_stream_object *self = (wi_stream_object *)self_;
-
-	delete self->pshared;
-
-	self->pbare = nullptr;
-	self->pshared = nullptr;
-	Py_TYPE(self)->tp_free((PyObject*) self);
-    }
-
-    // Get a bare pointer from a (PyObject *) which is known to be a wi_stream_object
-    static inline rf_pipelines::wi_stream *get_pbare(PyObject *obj)
-    {
-	wi_stream_object *s = (wi_stream_object *) obj;
-
-	if (s->pbare)
-	    return s->pbare;
-
-	PyErr_SetString(PyExc_RuntimeError, "rf_pipelines: internal error: unexpected NULL pointer");
-	return NULL;	
-    }
-
-    static PyObject *nfreq_getter(PyObject *self, void *closure)
-    {
-	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
-	return p ? Py_BuildValue("i", p->nfreq) : NULL;
-    }
-
-    static PyObject *nt_maxwrite_getter(PyObject *self, void *closure)
-    {
-	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
-	return p ? Py_BuildValue("i", p->nt_maxwrite) : NULL;
-    }
-
-    static PyObject *freq_lo_MHz_getter(PyObject *self, void *closure)
-    {
-	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
-	return p ? Py_BuildValue("d", p->freq_lo_MHz) : NULL;
-    }
-
-    static PyObject *freq_hi_MHz_getter(PyObject *self, void *closure)
-    {
-	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
-	return p ? Py_BuildValue("d", p->freq_hi_MHz) : NULL;
-    }
-
-    static PyObject *dt_sample_getter(PyObject *self, void *closure)
-    {
-	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
-	return p ? Py_BuildValue("d", p->dt_sample) : NULL;
-    }
-};
-
-
-static PyGetSetDef wi_stream_getseters[] = {
-    { (char *)"nfreq", wi_stream_object::nfreq_getter, NULL, NULL, NULL },
-    { (char *)"nt_maxwrite", wi_stream_object::nt_maxwrite_getter, NULL, NULL, NULL },
-    { (char *)"freq_lo_MHz", wi_stream_object::freq_lo_MHz_getter, NULL, NULL, NULL },
-    { (char *)"freq_hi_MHz", wi_stream_object::freq_hi_MHz_getter, NULL, NULL, NULL },
-    { (char *)"dt_sample", wi_stream_object::dt_sample_getter, NULL, (char *)"sample length in seconds", NULL },
-    { NULL, NULL, NULL, NULL, NULL }
-};
-
-
-static PyTypeObject wi_stream_type = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    "rf_pipelines_c.wi_stream",  /* tp_name */
-    sizeof(wi_stream_object),    /* tp_basicsize */
-    0,                         /* tp_itemsize */
-    wi_stream_object::tp_dealloc,  /* tp_dealloc */
-    0,                         /* tp_print */
-    0,                         /* tp_getattr */
-    0,                         /* tp_setattr */
-    0,                         /* tp_reserved */
-    0,                         /* tp_repr */
-    0,                         /* tp_as_number */
-    0,                         /* tp_as_sequence */
-    0,                         /* tp_as_mapping */
-    0,                         /* tp_hash  */
-    0,                         /* tp_call */
-    0,                         /* tp_str */
-    0,                         /* tp_getattro */
-    0,                         /* tp_setattro */
-    0,                         /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,   /* tp_flags */
-    "MY AWESOME STREAM",           /* tp_doc */
-    0,                         /* tp_traverse */
-    0,                         /* tp_clear */
-    0,                         /* tp_richcompare */
-    0,                         /* tp_weaklistoffset */
-    0,                         /* tp_iter */
-    0,                         /* tp_iternext */
-    0,                         /* tp_methods */
-    0,                         /* tp_members */
-    wi_stream_getseters,       /* tp_getset */
-};
-
-
-// make new python object (for factory functions)
-// can return NULL if something goes wrong
-static PyObject *make_wi_stream(const shared_ptr<rf_pipelines::wi_stream> &ptr)
-{
-    if (!ptr) {
-	PyErr_SetString(PyExc_RuntimeError, "rf_pipelines: internal error: empty pointer passed to make_wi_stream()");
-	return NULL;
-    }
-
-    wi_stream_object *ret = PyObject_New(wi_stream_object, &wi_stream_type);
-    if (!ret)
-	return NULL;
-
-    try {
-	ret->pbare = ptr.get();
-	ret->pshared = new shared_ptr<rf_pipelines::wi_stream> (ptr);
-    }
-    catch (...) {
-	Py_XDECREF(ret);
-	return PyErr_NoMemory();
-    }
-
-    return (PyObject *) ret;
-}
-
-
-// -------------------------------------------------------------------------------------------------
-//
-// Wrap wi_transform
+// wi_transform wrapper class
 
 
 // "Upcalling" transform whose virtual functions are implemented by python upcalls.
@@ -435,6 +295,146 @@ static PyObject *make_wi_transform(const shared_ptr<rf_pipelines::wi_transform> 
 
     try {
 	ret->pshared = new shared_ptr<rf_pipelines::wi_transform> (ptr);
+    }
+    catch (...) {
+	Py_XDECREF(ret);
+	return PyErr_NoMemory();
+    }
+
+    return (PyObject *) ret;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//
+// wi_stream wrapper class
+
+
+struct wi_stream_object {
+    PyObject_HEAD
+
+    // "Borrowed" reference (delete will not be called in tp_dealloc())
+    rf_pipelines::wi_stream *pbare;
+
+    // Reference held through shared_ptr.  Using a pointer-to-a-shared pointer was least awkward here.
+    shared_ptr<rf_pipelines::wi_stream> *pshared;
+
+    // note: no tp_alloc function is necessary, since the default (PyType_GenericAlloc()) calls memset().    
+    static void tp_dealloc(PyObject *self_)
+    {
+	wi_stream_object *self = (wi_stream_object *)self_;
+
+	delete self->pshared;
+
+	self->pbare = nullptr;
+	self->pshared = nullptr;
+	Py_TYPE(self)->tp_free((PyObject*) self);
+    }
+
+    // Get a bare pointer from a (PyObject *) which is known to be a wi_stream_object
+    static inline rf_pipelines::wi_stream *get_pbare(PyObject *obj)
+    {
+	wi_stream_object *s = (wi_stream_object *) obj;
+
+	if (s->pbare)
+	    return s->pbare;
+
+	PyErr_SetString(PyExc_RuntimeError, "rf_pipelines: internal error: unexpected NULL pointer");
+	return NULL;	
+    }
+
+    static PyObject *nfreq_getter(PyObject *self, void *closure)
+    {
+	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
+	return p ? Py_BuildValue("i", p->nfreq) : NULL;
+    }
+
+    static PyObject *nt_maxwrite_getter(PyObject *self, void *closure)
+    {
+	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
+	return p ? Py_BuildValue("i", p->nt_maxwrite) : NULL;
+    }
+
+    static PyObject *freq_lo_MHz_getter(PyObject *self, void *closure)
+    {
+	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
+	return p ? Py_BuildValue("d", p->freq_lo_MHz) : NULL;
+    }
+
+    static PyObject *freq_hi_MHz_getter(PyObject *self, void *closure)
+    {
+	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
+	return p ? Py_BuildValue("d", p->freq_hi_MHz) : NULL;
+    }
+
+    static PyObject *dt_sample_getter(PyObject *self, void *closure)
+    {
+	rf_pipelines::wi_stream *p = wi_stream_object::get_pbare(self);
+	return p ? Py_BuildValue("d", p->dt_sample) : NULL;
+    }
+};
+
+
+static PyGetSetDef wi_stream_getseters[] = {
+    { (char *)"nfreq", wi_stream_object::nfreq_getter, NULL, NULL, NULL },
+    { (char *)"nt_maxwrite", wi_stream_object::nt_maxwrite_getter, NULL, NULL, NULL },
+    { (char *)"freq_lo_MHz", wi_stream_object::freq_lo_MHz_getter, NULL, NULL, NULL },
+    { (char *)"freq_hi_MHz", wi_stream_object::freq_hi_MHz_getter, NULL, NULL, NULL },
+    { (char *)"dt_sample", wi_stream_object::dt_sample_getter, NULL, (char *)"sample length in seconds", NULL },
+    { NULL, NULL, NULL, NULL, NULL }
+};
+
+
+static PyTypeObject wi_stream_type = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "rf_pipelines_c.wi_stream",  /* tp_name */
+    sizeof(wi_stream_object),    /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    wi_stream_object::tp_dealloc,  /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "MY AWESOME STREAM",           /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    0,                         /* tp_methods */
+    0,                         /* tp_members */
+    wi_stream_getseters,       /* tp_getset */
+};
+
+
+// make new python object (for factory functions)
+// can return NULL if something goes wrong
+static PyObject *make_wi_stream(const shared_ptr<rf_pipelines::wi_stream> &ptr)
+{
+    if (!ptr) {
+	PyErr_SetString(PyExc_RuntimeError, "rf_pipelines: internal error: empty pointer passed to make_wi_stream()");
+	return NULL;
+    }
+
+    wi_stream_object *ret = PyObject_New(wi_stream_object, &wi_stream_type);
+    if (!ret)
+	return NULL;
+
+    try {
+	ret->pbare = ptr.get();
+	ret->pshared = new shared_ptr<rf_pipelines::wi_stream> (ptr);
     }
     catch (...) {
 	Py_XDECREF(ret);
