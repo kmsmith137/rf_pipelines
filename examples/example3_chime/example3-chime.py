846a6744d2a70504d6b36e19210f36149b1b3fe6
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-07-05 09:23:05 -0400

    add reference_raw_chime_0.png

--- examples/example3_chime/example3-chime.py
+++ examples/example3_chime/example3-chime.py
@@ -4,20 +4,25 @@ import os
 import rf_pipelines
 
 # Analyze four arbitrarily chosen files from the June 21 run.
-# The utility 'ch-show-intensity-file' may be useful for quickly inspecting a CHIME hdf5 file.
+# The filenames below assume you're running on chimer.physics.mcgill.ca
+# Note: The utility 'ch-show-intensity-file' may be useful for quickly inspecting a CHIME hdf5 file.
 filename_list = [ '00003014.h5', '00003035.h5', '00003056.h5', '00003078.h5' ]
 filename_list = [ os.path.join('/data/pathfinder/16-06-21',f) for f in filename_list ]
 
 # Construct CHIME stream object.
 s = rf_pipelines.chime_stream_from_filename_list(filename_list)
 
-# Currently 'rf_pipelines' contains a simple detrending transform, and no RFI-removing transforms!
-t1 = rf_pipelines.simple_detrender(1024)
+# This plotter_transform is before the detrender, so it generates "raw" (non-detrended)
+# plots.  Downsampling by a factor 16 in time, and using 1200 coarse-grained times per
+# waterfall plot, we end up with 4 plots (filenames raw_chime_0.png, raw_chime_1.png, ...)
 
-# We put the plotter_transform after the detrender, so that it incrementally generates
-# plots of detrended data.  Downsampling by a factor 16 in time, and using 1200 coarse-grained
-# times per waterfall plot, we get 4 waterfall plots in this stream.
-t2 = rf_pipelines.plotter_transform('detrended_chime', img_nfreq=512, img_nt=1200, downsample_nt=16)
+t1 = rf_pipelines.plotter_transform('raw_chime', img_nfreq=512, img_nt=1200, downsample_nt=16)
+
+# Currently 'rf_pipelines' contains a simple detrending transform, and no RFI-removing transforms!
+t2 = rf_pipelines.simple_detrender(1024)
+
+# This plotter_transform is after the detrender, so it generates detrended plots.
+t3 = rf_pipelines.plotter_transform('detrended_chime', img_nfreq=512, img_nt=1200, downsample_nt=16)
 
 # Dedisperse and write coarse-grained triggers to the file 'triggers.hdf5'.
 #
@@ -27,6 +32,6 @@ t2 = rf_pipelines.plotter_transform('detrended_chime', img_nfreq=512, img_nt=120
 # improvement, in particular if run on a large stream it will make a "monster" plot with a huge
 # number of pixels).
 
-t3 = rf_pipelines.bonsai_dedisperser('bonsai_config.hdf5', 'triggers.hdf5')
+t4 = rf_pipelines.bonsai_dedisperser('bonsai_config.hdf5', 'triggers.hdf5')
 
-s.run([t1,t2,t3])
+s.run([t1,t2,t3,t4])
