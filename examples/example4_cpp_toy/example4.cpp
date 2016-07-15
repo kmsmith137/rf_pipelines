99a2da7bf05af284220c9889a71a787bc2d8ff84
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-07-15 12:43:16 -0400

    more example4.cpp

--- examples/example4_cpp_toy/example4.cpp
+++ examples/example4_cpp_toy/example4.cpp
@@ -12,11 +12,11 @@ int main(int argc, char **argv)
     // First let's make the stream.  There are several experiment-specific streams defined in rf_pipelines.hpp, 
     // but we'll just use a simple stream which outputs Gaussian random numbers.
 
-    int nfreq = 1024;           // Number of frequency channels
-    int nt_tot = 32000;         // Number of time samples before end of stream (32 seconds of data, since dt_sample=10^-3)
-    double freq_lo_MHz = 400.;  // CHIME
-    double freq_hi_MHz = 800.;  // CHIME
-    double dt_sample = 1.0e-3;  // CHIME-ish
+    int nfreq = 1024;               // Number of frequency channels
+    int nt_tot = 32000;             // Number of time samples before end of stream
+    double freq_lo_MHz = 400.;      // CHIME
+    double freq_hi_MHz = 800.;      // CHIME
+    double dt_sample = 1.31072e-3;  // CHIME pathfinder
 
     shared_ptr<wi_stream> stream = make_gaussian_noise_stream(nfreq, nt_tot, freq_lo_MHz, freq_hi_MHz, dt_sample);
 
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
     vector< shared_ptr<wi_transform> > transform_list;
     
     // First, a simple detrending transform which is probably too simple for the real world!
-    transform_list.push_back(make_simple_detrender(1024));
+    transform_list.push_back( make_simple_detrender(1024) );
     
     // In a real pipeline, we'd put some RFI-removing transforms next, but these aren't implemented in C++ yet
     // (coming soon, let me know if you'd like to help) so this comment is a placeholder.
@@ -38,9 +38,23 @@ int main(int argc, char **argv)
 #if 1  // uncomment to enable
     string filename = "example4_data.hdf5";
     bool clobber = true;
-    transform_list.push_back(make_chime_file_writer(filename,clobber));
+    transform_list.push_back( make_chime_file_writer(filename,clobber) );
 #endif
 
+    // Now add a 'bonsai_transform', a pseudo-transform which dedisperses the data.
+    //
+    // Before this will work, you'll need to create the file 'bonsai_config.hdf5' from the
+    // configuration file 'bonsai_config.txt', using the command:
+    //    bonsai-mkweight bonsai_config.txt bonsai_config.hdf5
+    //
+    // To plot the output of the dedispersion transform, use the command:
+    //    bonsai-plot-triggers.py bonsai_outputs.hdf5
+    //
+    string bonsai_config_filename = "bonsai_config.hdf5";
+    string bonsai_output_filename = "bonsai_outputs.hdf5";
+    transform_list.push_back( make_bonsai_dedisperser(bonsai_config_filename, bonsai_output_filename) );
+
+    // Now that the stream and transforms have been set up, this line of code runs the pipeline!
     stream->run(transform_list);
 
     return 0;
