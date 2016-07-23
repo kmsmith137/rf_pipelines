1e6267108f7c5832751329d8019a0fb8accdc121
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-07-23 15:51:34 -0400

    fixed the assert loop; modified input arg; changed names for consistency

--- rf_pipelines/transforms/badchannel_mask.py
+++ rf_pipelines/transforms/badchannel_mask.py
@@ -7,47 +7,46 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
 
     Constructor syntax:
 
-      t = badchannel_mask(maskpath, freq_lo_MHz=400.0, freq_hi_MHz=800.0, nt_chunk=1024)
+      t = badchannel_mask(maskpath, nt_chunk=1024)
 
       'maskpath' is the full path to a mask file that contains affected freq 
       intervals, written in rows with the following format: e.g., 420.02,423.03
-
-      'freq_lo_MHz=400.0' and 'freq_hi_MHz=800.0' are the lowest and the highest 
-      possible feq for the mask, respectively.
 
       'nt_chunk=1024' is the buffer size, which is allowed to have a different 
       value in a chain of transforms.
     """
 
-    def __init__(self, maskpath, freq_lo_MHz=400.0, freq_hi_MHz=800.0, nt_chunk=1024):
+    def __init__(self, maskpath, nt_chunk=1024):
         
-        self.maskpath = maskpath
-        self.freq_lo_MHz = freq_lo_MHz
-        self.freq_hi_MHz = freq_hi_MHz
-        
+        self.maskpath = maskpath 
         self.nt_chunk = nt_chunk
         self.nt_prepad = 0
         self.nt_postpad = 0
         
         # Read the frequency mask into a numpy array.
         self.freq_mask = np.genfromtxt(self.maskpath, delimiter=',')
-        
-        # Now let's make sure all input arguments are valid.
-        assert self.freq_hi_MHz > self.freq_lo_MHz, "freq_hi_MHz must be greater than freq_lo_MHz."
-        i = 0
-        for k in self.freq_mask:
-            assert k[0] <= k[1], "The frequency interval %r in '%s' is not a valid input" % (k, self.maskpath)
-            # Remove useless intervals.
-            if ((k[0] < self.freq_lo_MHz) and (k[1] < self.freq_lo_MHz)) \
-                or ((k[0] > self.freq_hi_MHz) and (k[1] > self.freq_hi_MHz)):
-                    self.freq_mask = np.delete(self.freq_mask, i, 0)
-            i += 1
 
-    def set_stream(self, s):
+    def set_stream(self, stream):
 
         # self.nfreq corresponds to the number of freq channels in the chunk.
-        self.nfreq = s.nfreq
+        self.nfreq = stream.nfreq
         
+        # freq_lo_MHz and freq_hi_MHz are the lowest and the highest 
+        # possible feq for the mask, respectively.
+        self.freq_lo_MHz = stream.freq_lo_MHz
+        self.freq_hi_MHz = stream.freq_hi_MHz
+        
+        # Check whether all freq intervals are valid and whether they overlap 
+        # with the freq range of the stream. Keep only the ones that do overlap.
+        trimmed_mask = []
+        for (freq0,freq1) in self.freq_mask:
+            assert freq0 <= freq1, "The frequency interval %r in '%s' is not a valid input" \
+                % ([freq0,freq1], self.maskpath)
+            if (self.freq_lo_MHz <= freq0 <= self.freq_hi_MHz) \
+                or (self.freq_lo_MHz <= freq1 <= self.freq_hi_MHz):
+                    trimmed_mask.append([freq0,freq1])
+        self.freq_mask = np.array(trimmed_mask)
+
         # First we need to scale our frequency mask so that it matches with
         # the number of channels in the chunk. Then we make a copy of
         # the mask that will be processed and used as an array of indexes.        
@@ -73,7 +72,7 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
         # the upper bound.
         self.index_mask[self.index_mask < 0.] = int(0)
 
-        # The following is a private method for testing the class
+        # The following is a private method for testing the class.
         self._badchannel_mask__test()
 
     def __test(self):
@@ -84,5 +83,5 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
         # Here we loop over bad frequency intervals. Note that index 
         # values have to be used in the reversed order since we already 
         # subtracted the max value from the mask.
-        for k in self.index_mask:
-            weights[k[1]:k[0],:] = 0.
+        for (freq1,freq0) in self.index_mask:
+            weights[freq0:freq1,:] = 0.
