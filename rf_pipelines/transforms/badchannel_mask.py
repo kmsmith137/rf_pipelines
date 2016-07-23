4b7a6149a9d8f3fae303732dd03c291fa3478e11
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-07-22 23:39:51 -0400

    incl priv method for test

--- rf_pipelines/transforms/badchannel_mask.py
+++ rf_pipelines/transforms/badchannel_mask.py
@@ -39,24 +39,24 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
             assert k[0] <= k[1], "The frequency interval %r in '%s' is not a valid input" % (k, self.maskpath)
             # Remove useless intervals.
             if ((k[0] < self.freq_lo_MHz) and (k[1] < self.freq_lo_MHz)) \
-            or ((k[0] > self.freq_hi_MHz) and (k[1] > self.freq_hi_MHz)):
-                self.freq_mask = np.delete(self.freq_mask, i, 0)
+                or ((k[0] > self.freq_hi_MHz) and (k[1] > self.freq_hi_MHz)):
+                    self.freq_mask = np.delete(self.freq_mask, i, 0)
             i += 1
 
     def set_stream(self, s):
 
         # self.nfreq corresponds to the number of freq channels in the chunk.
         self.nfreq = s.nfreq
-
+        
         # First we need to scale our frequency mask so that it matches with
         # the number of channels in the chunk. Then we make a copy of
         # the mask that will be processed and used as an array of indexes.        
-        scale = self.nfreq / (self.freq_hi_MHz - self.freq_lo_MHz)
-        self.index_mask = self.freq_mask * scale
+        self.scale = self.nfreq / (self.freq_hi_MHz - self.freq_lo_MHz)
+        self.index_mask = self.freq_mask * self.scale
 
         # Subtracting the max value from the mask (which runs from low
         # to high values) leaves us with an array that runs in reverse.
-        self.index_mask = (self.freq_hi_MHz * scale) - self.index_mask
+        self.index_mask = (self.freq_hi_MHz * self.scale) - self.index_mask
 
         # This part is a bit tricky! First we make sure that we include
         # any non-zero overlap with the mask. To this end, we take the
@@ -66,12 +66,18 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
         # so they can be feed as indexes into the weights array.
         self.index_mask[:,0] = (np.ceil(self.index_mask[:,0])).astype(int)
         self.index_mask[:,1] = (np.floor(self.index_mask[:,1])).astype(int)
-        
+       
+        # The following is a private method for testing the class
+        self._badchannel_mask__test()
+
         # This line is to make sure that we don't use negative
         # indexes in the lower bound. Numpy arrays however accept
         # numbers beyond the maximum index -- so no need to constrain
         # the upper bound.
         self.index_mask[self.index_mask < 0.] = int(0)
+
+    def __test(self):
+        pass
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         
