af1c459766ba7713fa9c21d010fe1a6cb97fce0e
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-07-30 22:11:31 -0400

    fixed neg inten; asserted; commented

--- rf_pipelines/transforms/clipper_transform.py
+++ rf_pipelines/transforms/clipper_transform.py
@@ -3,16 +3,21 @@ import rf_pipelines
 
 class clipper_transform(rf_pipelines.py_wi_transform):
     """
-   Clips the intensity above a given stdv threshold. 
-   Results are applied to the weights array for masking RFIs.
+   Clips the intensity along a selected axis and 
+   above a given threshold. Results are applied to 
+   the weights array (i.e., weights[clipped] = 0.) 
+   for masking extreme values.
+
+   Currently based on the standard deviation from
+   the mean (np.std)
 
     Constructor syntax:
 
       t = clipper_transform(thr=3, axis=0, nt_chunk=1024)
 
       'thr=3.' is the multiplicative factor of maximum threshold,
-        e.g., 3 * standard_deviation, meaning that anything above this
-        limit is clipped.
+        e.g., 3 * standard_deviation, meaning that any sample
+        above/below this limit is clipped.
 
       'axis=0' is the axis convention:
         0: along freq; constant time
@@ -31,19 +36,35 @@ class clipper_transform(rf_pipelines.py_wi_transform):
 
         assert (self.axis == 0 or self.axis == 1), \
         'axis must be 0 (along freq; constant time) or 1 (along time; constant freq).'
+        assert self.thr >= 1., 'threshold must be >= 1.'
 
     def set_stream(self, stream):
 
         self.nfreq = stream.nfreq
-
+        
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         
+        # Compute the standard deviation of the intensity 
+        # array (2d) along the selected axis. The result is 
+        # a 1d array with a size equivalent to the number of 
+        # elements along the other axis.
         self.clip = np.std(intensity, axis=self.axis)
-         
+
+        # The idea here is to dimensionally match the self.clip 
+        # array (1d) with the intensity (2d) and weights (2d)
+        # so that their elements pick up a one-to-one logical 
+        # correspondance. To this end, we tile (in-place) self.clip 
+        # by copying the 1d array along the selected axis.
         if self.axis == 0:
             self.clip = np.tile(self.clip, (self.nfreq, 1))
         else:
             self.clip = np.transpose(np.tile(self.clip, (self.nt_chunk, 1)))
         
+        # Verify that all the arrays have the same dimensions.
         assert np.shape(weights) == np.shape(intensity) == np.shape(self.clip)
-        weights[ intensity > (self.thr*self.clip) ] = 0.
+
+        # Assign zero weights to those elements that have an
+        # intensity value above/below the threshold limit.
+        # The statement in [ ] creates a boolean array which
+        # picks up elements in the weights array.
+        weights[ np.abs(intensity) > (self.thr*self.clip) ] = 0.
