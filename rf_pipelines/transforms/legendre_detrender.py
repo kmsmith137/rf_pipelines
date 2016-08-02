4c740d4c8d4a95bd66d9bf41fd66458e824536dc
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-08-01 20:01:22 -0400

    first implementation of the algorithm; untested code

--- rf_pipelines/transforms/legendre_detrender.py
+++ rf_pipelines/transforms/legendre_detrender.py
@@ -1,6 +1,5 @@
 import numpy as np
 import rf_pipelines
-import numpy.polynomial.legendre as npl
 
 class legendre_detrender(rf_pipelines.py_wi_transform):
     """
@@ -42,3 +41,41 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
         self.nfreq = stream.nfreq
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
+        
+        # <<<<<<< Assuming self.axis = 0 >>>>>>>
+        # --------------------------------------
+        # A.1 precomputing P(x_i)_alpha; TODO to be moved to set_stream?
+        # TODO have to create an inner func taking nfreq/nt_chunk as input
+        x = np.linspace(-1, 1, self.nfreq) # TODO could use .tile to combine with p 
+        coef = np.eye(self.deg+1)
+        p = np.zeros(self.deg+1, self.nfreq)
+
+        for d in xrange(self.deg+1):
+            p[d,:] = np.polynomial.Legendre(coef[d,:])(x)
+        
+        # --------------------------------------
+        # A.2 looping over time samples
+        assert np.shape(weights)[self.axis] == np.shape(p)[1] # use self.axis convention
+        # TODO change iterator `t' to sample/ sth else)
+        for t in xrange(self.nt_chunk):
+            
+            # TODO weights[:,t] vs. weights[n,:] depending on self.axis
+            M = np.dot( weights[:,t] * p, p.T) # (d+1) by (d+1)
+            assert np.size(M) == (self.deg+1, self.deg+1) # TODO check if necessary
+            
+            v = np.sum( weights[:,t] * intensity[:,t] * p, axis=1) # (d+1) by 1
+            assert np.size(v) == (self.deg+1, 1) # TODO check if necessary
+
+            # --------------------------------------
+            # A.3 conditional statements
+            # TODO if poorly conditioned, then skip, else continue..
+            
+            # --------------------------------------
+            # A.4
+            c = np.linalg.inv(M) * v
+            assert np.size(c) == self.deg+1 # TODO check if necessary
+            fit = np.polynomial.Legendre(c)(x)
+            
+            # --------------------------------------
+            # A.5 TODO: what to do with fit? e.g., TODO tailor to `self.axis'
+            intensity[:,t] -= fit # TODO combine with A.4 to save memory
