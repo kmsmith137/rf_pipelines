c4811738018c7b29a63318731c325c80ec3a894c
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-08-10 23:20:12 -0400

    enabled 2d mode

--- rf_pipelines/transforms/clipper_transform.py
+++ rf_pipelines/transforms/clipper_transform.py
@@ -4,18 +4,19 @@ import rf_pipelines
 class clipper_transform(rf_pipelines.py_wi_transform):
     """
    This transform clips the intensity along a selected 
-   axis and above a given threshold. Results are applied 
-   to the weights array (i.e., weights[clipped] = 0.) 
-   for masking extreme values.
+   axis -- also works in planar (2d) mode -- and above 
+   a given threshold. Results are applied to the weights 
+   array (i.e., weights[clipped] = 0.) for masking 
+   extreme values.
 
-   - Currently based on the weighted standard deviation
-   as explained in "chime_zerodm_notes".
-   - Assumes zero mean (i.e., the intensity has already 
+   + Assumes zero mean (i.e., the intensity has already 
    been detrended along the selected axis).
-   
+   + Currently based on the weighted standard deviation 
+   as explained in "chime_zerodm_notes".
+    
     Constructor syntax:
 
-      t = clipper_transform(thr=3, axis=0, nt_chunk=1024)
+      t = clipper_transform(thr=3, axis=0, nt_chunk=1024, test=False)
 
       'thr=3.' is the multiplicative factor of maximum threshold,
         e.g., 3 * standard_deviation, meaning that (the absolute
@@ -24,21 +25,26 @@ class clipper_transform(rf_pipelines.py_wi_transform):
       'axis=0' is the axis convention:
         0: along freq; constant time
         1: along time; constant freq
+        2: planar; freq and time
 
       'nt_chunk=1024' is the buffer size.
+
+      'test=False' enables a test mode.
+
     """
 
-    def __init__(self, thr=3., axis=0, nt_chunk=1024):
+    def __init__(self, thr=3., axis=0, nt_chunk=1024, test=False):
         
         self.thr = thr
         self.axis = axis
         self.nt_chunk = nt_chunk
         self.nt_prepad = 0
         self.nt_postpad = 0
+        self.test = test
 
-        assert (self.axis == 0 or self.axis == 1),\
-            'axis must be 0 (along freq; constant time) or 1 (along time; constant freq).'
-        assert self.thr >= 1., 'threshold must be >= 1.'
+        assert (self.axis == 0 or self.axis == 1 or self.axis == 2),\
+            "axis must be 0 (along freq; constant time), 1 (along time; constant freq), or 2 (planar; freq and time)"
+        assert self.thr >= 1., "threshold must be >= 1."
 
     def set_stream(self, stream):
 
@@ -51,38 +57,44 @@ class clipper_transform(rf_pipelines.py_wi_transform):
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         
-        # --->>> The following is a private method for testing the class.
-        #weights, intensity = self._clipper_transform__test(weights, intensity)
+        if self.test:
+            weights, intensity = self._clipper_transform__test(weights, intensity)
         
-        # This 1d array holds the sum of weights along
-        # the selected axis. Its size is equal to the
-        # unselected axis.
-        self.sum_weights = np.sum(weights, axis=self.axis)
+        if self.axis != 2: # 1d mode
+            # This 1d array holds the sum of weights along
+            # the selected axis. Its size is equal to the
+            # unselected axis.
+            self.sum_weights = np.sum(weights, axis=self.axis)
 
-        # Let's tile our 1d array by using 
-        # self.tile_arr() so that its dimensions
-        # (now in 2d; [nfreq, nt_chunk]) match 
-        # with intensity, weights, and self.clip.
-        self.sum_weights = self.tile_arr(self.sum_weights)
+            # Let's tile our 1d array by using 
+            # self.tile_arr() so that its dimensions
+            # (now in 2d; [nfreq, nt_chunk]) match 
+            # with intensity, weights, and self.clip.
+            self.sum_weights = self.tile_arr(self.sum_weights)
         
-        # Here is an element-by-element operation (2d).
-        # Note that np.sum(2d) results in a 1d array. Therefore,
-        # we have to use self.tile_arr() to make the 2d elements 
-        # one-to-one.
-        indx, indy = np.where(self.sum_weights > 0.)
-        self.clip[indx,indy] = np.sqrt(self.tile_arr(\
-            np.sum(weights*(intensity)**2,\
-            axis=self.axis))[indx,indy]/\
-            self.sum_weights[indx,indy])
+            # Here is an element-by-element operation (2d).
+            # Note that np.sum(2d) results in a 1d array. Therefore,
+            # we have to use self.tile_arr() to make the 2d elements 
+            # one-to-one.
+            indx, indy = np.where(self.sum_weights > 0.)
+            self.clip[indx,indy] = np.sqrt(self.tile_arr(\
+                np.sum(weights*(intensity)**2,\
+                axis=self.axis))[indx,indy]/\
+                self.sum_weights[indx,indy])
+        else:
+            # 2d mode
+            self.sum_weights = np.sum(weights)
+            if self.sum_weights != 0.:
+                self.clip[:] = np.sqrt(np.sum(weights*(intensity)**2)/self.sum_weights)
 
         # Assign zero weights to those elements that have an
         # intensity value beyond the threshold limit.
         assert weights.shape == intensity.shape == self.clip.shape
         np.putmask(weights, np.abs(intensity) > (self.thr*self.clip), 0.)
         
-        # --->>> This is part of the test run.
-        #print np.count_nonzero(weights) /\
-        #        float(self.nfreq*self.nt_chunk) * 100, "% not masked."
+        if self.test:
+            print np.count_nonzero(weights) /\
+                float(self.nfreq*self.nt_chunk) * 100, "% not masked."
         
     def tile_arr(self, arr):
         # This method tiles (i.e., copies) a 1d array along the 
