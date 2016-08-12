5b92473e142f409ae2850e218f9126df52566185
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-08-11 20:19:05 -0400

    test is complete (passed)

--- rf_pipelines/transforms/legendre_detrender.py
+++ rf_pipelines/transforms/legendre_detrender.py
@@ -62,6 +62,8 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
     
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         
+        # The test mode replaces the weights and intensity with
+        # simulated chunks (see __test below).
         if self.test:
             weights, intensity = self._legendre_detrender__test(weights, intensity)
 
@@ -81,9 +83,13 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
                 intensity[n,:] -= self.leg_fit(weights[n,:], intensity[n,:])
 
         if self.test:
+            # Print the degree of fit, the weighted mean 
+            # and stdv computed over the entirety of each 
+            # chunk. This shows the goodness of our detrender.
             mean = np.sum(weights*intensity)/np.sum(weights)
-            rms = np.sqrt(np.sum(weights*(intensity-mean)**2)/np.sum(weights))
-            print (self.deg, mean, rms), " = (deg, mean, rms)"
+            stdv = np.sqrt(np.sum(weights*(intensity-mean)**2)/np.sum(weights))
+            print (self.deg, mean, stdv), "= (deg, mean, stdv)"
+            print "#####################################################"
 
     def leg_fit(self, w, i):
         """This method computes the coefficients of the 
@@ -102,8 +108,9 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
         # (see "chime_zerodm_notes").
         if np.sum(w) == 0.:
             return 0.
+
         else:
-            # The following is explained in the notes.
+            # The following section is explained in "chime_zerodm_notes".
             M = np.dot(w * self.P, self.P.T)
             assert np.shape(M) == (self.deg+1, self.deg+1)
             v = np.sum(w * i * self.P, axis=1)
@@ -111,24 +118,42 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
             # method for catching an ill-conditioned M.
             c = np.dot(np.linalg.inv(M), v)
             assert np.size(c) == self.deg+1
+            
+            # Print the computed coeff of the best fit.
+            # Warning: a very long print! 
+            # But this is proven to be a very useful probe 
+            # for catching inconsistent outputs in test(_mode=1).
+            if self.test:
+                print c, "= Computed Coeff"
+
+            # Use self.P to evaluate the poly (defined by c)
+            # over the fit domain.
             return np.dot(c, self.P)
     
-    def __test(self, weights, intensity, mask_level=0.3, test_mode=1):
+    def __test(self, weights, intensity, mask_level=0.9, test_mode=1):
         """This private method replaces the weights and intensity
-        arrays with a new set of pre-defined arrays (see below)"""
+        arrays with a new set of simulated arrays.
+        
+        + Search for the following words in the printed output on screen:
+        "Masked", "deg", "mean", "stdv", "Actual", "Computed"
+        + Set 'mask_level' to a float between -1 and +1 (e.g., mask_level=0
+        masks out ~50% of the weights.
+        + A visual test: In the main loop above, change '-=' to '=' so that
+        the output of the leg_fit() gets stored as the data.
+        """
 
-        # Let's create a weights array using a gaussian noise distribution.
+        # Let's create a weights array using a gaussian dist.
         weights[:] = np.random.normal(0, 1, weights.size).reshape(weights.shape)
         # Mask out weights less than 'mask_level'
         indx, indy = np.where(weights < mask_level)
         weights[indx,indy] = 0.
-        print indx.size / float(weights.size) * 100, " % masked"
+
+        print indx.size / float(weights.size) * 100, " % Masked"
 
         if test_mode == 0:
             # Fitting a noise level with randomly masked 
             # elements should result in a consistent chain 
-            # of outputs with different degrees of fit.
-            # Test the code by running the transform 
+            # of outputs. Test the code by running the transform 
             # (while self.test=True) with a few different 
             # values for 'self.deg'.
             intensity[:] = np.random.normal(0, 1, intensity.size).reshape(intensity.shape)
@@ -137,9 +162,26 @@ class legendre_detrender(rf_pipelines.py_wi_transform):
             # Test by fitting a well-defined poly tiled in 2d.
             # The poly's coefficients are chosen randomly 
             # (centered at 0 with stdv=10)
-            c = np.random.normal(0, 10, self.deg+1)
-            intensity[:] = rf_pipelines.tile_arr(\
-                    np.dot(c, self.P), self.axis,\
-                    self.nfreq, self.nt_chunk)
+            rc = np.random.normal(0, 10, self.deg+1)
+            
+            # Tricky! we have to tile along the unselected
+            # axis because our polynomial is already in 
+            # the direction of the selected axis.
+            if self.axis == 0:
+                tile_axis = 1
+            else:
+                tile_axis = 0
 
+            # Print the actual coefficients, which change 
+            # for each chunk -- the random generator gets 
+            # called in the loop over the unselected axis.
+            print rc, "= Actual Coeff"
+            
+            # Replace all intensity values by the 2d-tiled
+            # simulated chunk.
+            intensity[:] = rf_pipelines.tile_arr(\
+                    np.dot(rc, self.P), tile_axis,\
+                    self.nfreq, self.nt_chunk)
+        
+        # Simulated weights and intensity arrays
         return weights, intensity
