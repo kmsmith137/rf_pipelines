7524abeeb21eebb5b3984e75ce3dd9cd7547ec85
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-08-31 17:37:42 -0400

    add outdir_manager.cpp (forgot this in a previous commit)

---
+++ outdir_manager.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <sstream>
+#include "rf_pipelines_internals.hpp"
+
+using namespace std;
+
+namespace rf_pipelines {
+#if 0
+}; // pacify emacs c-mode
+#endif
+
+    
+outdir_manager::outdir_manager(const string &outdir_, bool clobber_ok_) :
+    clobber_ok(clobber_ok_)
+{
+    // Ensure trailing slash
+    if (outdir_.size() == 0)
+	this->outdir = "./";
+    else if (outdir_.back() == '/')
+	this->outdir = outdir_;
+    else
+	this->outdir = outdir_ + "/";
+
+    makedirs(outdir);
+	
+    if (clobber_ok)
+	return;
+
+    for (const string &s: listdir(outdir)) {
+	if (is_json_basename(s))
+	    throw runtime_error("Directory \"" + outdir + "\" contains stray files of the form pipeline_NN.json, and 'clobber' flag wasn't set");
+    }
+}
+
+
+// Returns the full pathname
+string outdir_manager::add_file(const string &basename)
+{
+    bool is_new = basename_set.insert(basename).second;
+    string ret = outdir + basename;
+
+    if (!is_new)
+	throw runtime_error("rf_pipelines: output file '" + ret + "' was written twice in same pipeline run");
+
+    return ret;
+}
+
+
+void outdir_manager::write_per_substream_json_file(int isubstream, const Json::Value &data, bool noisy)
+{
+    if (isubstream < 0)
+	throw runtime_error("outdir_manager::write_json_file(): 'isubstream' arg was negative");
+
+    stringstream ss;
+    ss << this->outdir << "pipeline_" << isubstream << ".json";
+
+    string filename = ss.str();
+    ofstream f(filename);
+
+    if (f.fail())
+	throw runtime_error("couldn't open output file " + filename);
+
+    Json::StyledWriter w;
+    f << w.write(data);
+
+    if (noisy)
+	cerr << ("wrote " + filename + "\n");
+}
+
+
+// Static member function
+bool outdir_manager::is_json_basename(const string &basename)
+{
+    if (!startswith(basename, "pipeline_"))
+	return false;
+    if (!endswith(basename, ".json"))
+	return false;
+    
+    const char *s = basename.c_str();
+    int len = strlen(s);
+    
+    if (len <= 14)
+	return false;
+    
+    for (int i = 9; i < len-5; i++)
+	if (!isdigit(s[i]))
+	    return false;
+
+    return true;
+}
+    
+
+}  // namespace rf_pipelines
