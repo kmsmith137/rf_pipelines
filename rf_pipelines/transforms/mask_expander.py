2a3f61327b51f68e78768d09ebd7f33fd49bea36
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-09-15 17:12:29 -0400

    factor mask_expander unit test to its own source file

--- rf_pipelines/transforms/mask_expander.py
+++ rf_pipelines/transforms/mask_expander.py
@@ -1,6 +1,23 @@
 import numpy as np
 import rf_pipelines
 
+
+def expand_mask(weights, thr, axis):
+    """Helper function for mask_expander.  Modifies 'weights' array in place."""
+
+    (nfreq, nt_chunk) = weights.shape
+
+    # Compute the mean of weights along the seleted axis.
+    w_mean = np.mean(weights, axis=axis)
+
+    if axis is None:
+        if w_mean <= thr:
+            weights[:] = 0.
+    else:
+        w_mean = rf_pipelines.tile_arr(w_mean, axis, nfreq, nt_chunk)
+        np.putmask(weights, w_mean <= thr, 0.)
+
+
 class mask_expander(rf_pipelines.py_wi_transform):
     """
    This transform expands the mask by filling the weights 
@@ -24,11 +41,9 @@ class mask_expander(rf_pipelines.py_wi_transform):
         1: along time; constant freq.
       
       'nt_chunk=1024' is the buffer size.
-
-      'test=False' enables a test mode.
     """
 
-    def __init__(self, thr=0.2, axis=None, nt_chunk=1024, test=False):
+    def __init__(self, thr=0.2, axis=None, nt_chunk=1024):
         
         assert (0 < thr < 1), "threshold must be between 0 and 1."
         assert (axis == None) or (axis == 0) or (axis == 1),\
@@ -37,58 +52,9 @@ class mask_expander(rf_pipelines.py_wi_transform):
         self.thr = thr
         self.axis = axis
         self.nt_chunk = nt_chunk
-        self.test = test
 
-    def set_stream(self, stream):
-        
+    def set_stream(self, stream):        
         self.nfreq = stream.nfreq
 
-        if self.test:
-            # 1D mode: 'self.nmax' specifies the max-1 number 
-            # of elements along the unselected axis.
-            if self.axis == 0:
-                self.nmax = self.nt_chunk
-            elif self.axis == 1:
-                self.nmax = self.nfreq
-            # 2D mode: 
-            # 'self.stats' += [# of processed chunks, fraction of unmasked weights]
-            else:
-                self.stats = [1, 0]
-
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
-        # If 'test' is specified, this will be a pseudo-transform which doesn't modify weights
-        # in the pipeline, but simulates some fake weights and reports the masked fraction.
-        if self.test:
-            # 1D mode: replace the weights with equally-spaced
-            # (and linearly increasing) numbers between 0 and 1.
-            if self.axis != None:
-                weights = np.linspace(0., 1., self.nmax)
-                weights = rf_pipelines.tile_arr(weights, self.axis, self.nfreq, self.nt_chunk)
-            # 2D mode: replace the weights with some Gaussian random
-            # numbers with (mean, stdv) = (self.thr, 0.01).
-            else:
-                weights = np.random.normal(self.thr, 0.01, size=weights.shape)
-
-        # Compute the mean of weights along the seleted axis.
-        w_mean = np.mean(weights, axis=self.axis)
-
-        if self.axis is None:
-            if w_mean <= self.thr:
-                weights[:] = 0.
-        else:
-            w_mean = rf_pipelines.tile_arr(w_mean, self.axis, self.nfreq, self.nt_chunk)
-            np.putmask(weights, w_mean <= self.thr, 0.)
-
-
-        if self.test:
-            unmasked_fraction = np.count_nonzero(weights_origin) / float(weights_origin.size)
-            if self.axis != None:
-                print unmasked_fraction * 100., "% of the current weights are not masked."
-            else:
-                self.stats[0] += 1 
-                self.stats[1] += unmasked_fraction
-                print "===================================="
-                print "[# of processed chunks, fraction of unmasked weights] =", self.stats
-                print "This must approach 50%; try smaller nt_chunk for better statistics:"
-                print self.stats[1]/self.stats[0] * 100., "% of the processed chunks have been unmasked"
-                print "===================================="
+        expand_mask(weights, self.thr, self.axis)
