43616106cf3bf89b06810add9b2a1af1d51ede9f
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-11-01 22:53:39 -0400

    exp sample cleanup

--- examples/example3_chime/kendrick-experiment.py
+++ examples/example3_chime/kendrick-experiment.py
@@ -22,18 +22,20 @@ if not os.path.exists('bonsai_config.hdf5'):
 # Note: The utility 'ch-show-intensity-file' may be useful for quickly inspecting a CHIME hdf5 file.
 #       and the utility 'ch-show-intensity-file' makes a quick waterfall plot.
 
-run = 1
+run = 2
 
 if run == 1:
-    #filename_list = sorted(glob.glob('/data/pathfinder/16-07-08/*.h5'))
+    filename_list = sorted(glob.glob('/data/pathfinder/16-07-08/*.h5'))
+    
     filename_list = [ '00000147.h5', '00000163.h5', '00000180.h5', '00000196.h5']
     #filename_list = [ '00002326.h5', '00002342.h5', '00002359.h5', '00002375.h5', '00002392.h5', '00002408.h5', '00002424.h5' ]
     filename_list = [ os.path.join('/data/pathfinder/16-07-08',f) for f in filename_list ]
     
 if run == 2:
     filename_list = sorted(glob.glob('/data/pathfinder/16-09-19-incoherent-without-noise-source/*.h5'))[0:13]
-    #filename_list = [ '00000327.h5', '00000344.h5', '00000360.h5', '00000376.h5', '00000393.h5', '00000409.h5', '00000426' ]
-    #filename_list = [ os.path.join('/data/pathfinder/16-09-19-incoherent-without-noise-source/*.h5',f) for f in filename_list ]
+    
+    filename_list = [ '00000327.h5', '00000344.h5', '00000360.h5', '00000376.h5', '00000393.h5', '00000409.h5', '00000426.h5' ]
+    filename_list = [ os.path.join('/data/pathfinder/16-09-19-incoherent-without-noise-source',f) for f in filename_list ]
 
 print filename_list
 
