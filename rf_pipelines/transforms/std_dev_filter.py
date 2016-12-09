03045490b75877030bb62c72409e3c61e602060e
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-12-08 20:57:55 -0500

    moved asserts, enabled axes, enabled coarse_grained

--- rf_pipelines/transforms/std_dev_filter.py
+++ rf_pipelines/transforms/std_dev_filter.py
@@ -1,11 +1,41 @@
 import numpy as np
 import rf_pipelines
 
-def filter_stdv(intensity, weights, thr, axis):
+def filter_stdv(intensity, weights, thr, axis, dsample_nfreq, dsample_nt):
     """Helper function for std_dev_filter. Modifies 'weights' array in place."""
-    # TODO enable nt_dsample
+    
     (nfreq, nt_chunk) = intensity.shape
     
+    # Helper assertion calls
+    assert thr >= 1., "threshold must be >= 1."
+    assert (axis == 0 or axis == 1),\
+        "axis must be 0 (along freq; constant time), or 1 (along time; constant freq)."
+    assert nt_chunk > 0
+
+    assert assert (dsample_nt is None or dsample_nt > 0), "Invalid downsampling number along the time axis!"
+    assert (dsample_nfreq is None or dsample_nfreq > 0), "Invalid downsampling number along the freq axis!"
+
+    # Helper 'set_stream' calls
+    coarse_grained = (dsample_nfreq < nfreq) or (dsample_nt < nt_chunk)
+
+    if dsample_nfreq is None:
+        dsample_nfreq = nfreq
+    if dsample_nt is None:
+        dsample_nt = nt_chunk
+
+    if nfreq % dsample_nfreq != 0:
+        raise RuntimeError("filter_stdv: current implementation requires 'dsample_nfreq' to be a divisor of stream nfreq.")
+    if nt_chunk % dsample_nt != 0:
+        raise RuntimeError("filter_stdv: current implementation requires 'dsample_nt' to be a divisor of 'nt_chunk'.")
+
+    # Let's make a ref to the original high-resolution weights.
+    weights_hres = weights
+
+    if coarse_grained:
+        # Downsample the weights and intensity.
+        (intensity, weights) = rf_pipelines.wi_downsample(intensity, weights,\
+                dsample_nfreq, dsample_nt)
+
     # Compute the weighted standard deviation of the intensity
     # array along the selected axis.
     num = np.asarray(np.sum(weights*(intensity)**2, axis=axis))
@@ -23,7 +53,13 @@ def filter_stdv(intensity, weights, thr, axis):
     sd_stdv = rf_pipelines.tile_arr(sd.std(axis=axis), axis, nfreq, nt_chunk)
     sd_mean = rf_pipelines.tile_arr(sd.mean(axis=axis), axis, nfreq, nt_chunk)
 
-    np.putmask(weights, np.abs(sd-sd_mean) > (thr*sd_stdv), 0.)
+    # Boolean array which is True for masked values
+    mask = np.abs(sd-sd_mean) > (thr*sd_stdv)
+
+    if coarse_grained:
+        mask = rf_pipelines.upsample(mask, nfreq, nt_chunk)
+
+    np.putmask(weights, mask, 0.)
 
 class std_dev_filter(rf_pipelines.py_wi_transform):
     """
@@ -37,20 +73,17 @@ class std_dev_filter(rf_pipelines.py_wi_transform):
       'thr=3.' is the sigma value to clip. 
 
       'axis=0' is the axis convention:
-        None: planar; freq and time. 
         0: along freq; constant time.
         1: along time; constant freq.
 
       'nt_chunk=1024' is the buffer size.
+      
+      TODO comment on 'dsample_nfreq', 'dsample_nt'
+      TODO axis = None?
     """
 
     def __init__(self, thr=3., axis=None, nt_chunk=1024):
         
-        assert thr >= 1., "threshold must be >= 1."
-        assert (axis == None or axis == 0 or axis == 1),\
-            "axis must be None (planar; freq and time), 0 (along freq; constant time), or 1 (along time; constant freq)."
-        assert nt_chunk > 0
-
         self.thr = thr
         self.axis = axis
         self.nt_chunk  = nt_chunk
@@ -64,4 +97,4 @@ class std_dev_filter(rf_pipelines.py_wi_transform):
         self.nfreq = stream.nfreq
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
-        filter_stdv(intensity, weights, self.thr, self.axis)
+        filter_stdv(intensity, weights, self.thr, self.axis, dsample_nfreq, dsample_nt)
