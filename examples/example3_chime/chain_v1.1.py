6591835a4bdbf35b0fd284267371eea59ee08d90
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2016-12-08 19:56:26 -0500

    cosmetics

--- examples/example3_chime/chain_v1.1.py
+++ examples/example3_chime/chain_v1.1.py
@@ -20,20 +20,14 @@ nc = 2
 nd = 2
 
 s = rf_pipelines.chime_stream_from_filename_list(filename_list, nt_chunk=1024, noise_source_align=detrend_nt)
-frb = rf_pipelines.frb_injector_transform(snr=1000, undispersed_arrival_time=1045.0, sample_rms=0.05, dm=500.)
+#frb = rf_pipelines.frb_injector_transform(snr=1000, undispersed_arrival_time=1045.0, sample_rms=0.05, dm=500.)
 
-def make_c_chain(ix):
-    return [ rf_pipelines.clipper_transform(thr=3, nt_chunk=clipper_nt, dsample_nfreq=512, dsample_nt=clipper_nt/16),
-             rf_pipelines.clipper_transform(thr=3, axis=0, nt_chunk=clipper_nt),
-             rf_pipelines.clipper_transform(thr=3, axis=1, nt_chunk=clipper_nt),
-             rf_pipelines.std_dev_filter(thr=3, axis=1, nt_chunk=clipper_nt/16) ]
-             
 transform_chain = [ frb, rf_pipelines.plotter_transform('raw', img_nfreq=512, img_nt=1200, downsample_nt=16),
                     rf_pipelines.badchannel_mask('/data/pathfinder/rfi_masks/rfi_20160705.dat', nt_chunk=clipper_nt) ] 
 
 for jx in xrange(nd):
     for ix in xrange(nc):
-        transform_chain += make_c_chain(ix)
+        transform_chain += master_clipper(clipper_nt=clipper_nt)
     transform_chain += [ rf_pipelines.legendre_detrender(deg=4, axis=1, nt_chunk=detrend_nt), 
                          rf_pipelines.legendre_detrender(deg=8, axis=0, nt_chunk=detrend_nt), 
                          rf_pipelines.bonsai_dedisperser('bonsai_config.hdf5', 'triggers%d.hdf5' % jx, nt_per_file=16*1200),
--- rf_pipelines/transforms/chain_iterator.py
+++ rf_pipelines/transforms/chain_iterator.py
@@ -1,20 +1,20 @@
 import numpy as np
 import rf_pipelines
 
-class chain_iterator(rf_pipelines.py_wi_transform):
+class master_clipper(rf_pipelines.py_wi_transform):
     """
-    chain clippers to enable global constraints
     """
-    def __init__(self, nt_chunk, clipper_nt, nc):
+    def __init__(self, nt_chunk, clipper_nt):
         self.nt_chunk = nt_chunk
         self.clipper_nt = clipper_nt
-        self.nc = nc
 
     def set_stream(self, stream):
         self.nfreq = stream.nfreq
     
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         
+        # global constraints....
+
         # clipper chain
         rf.pipelines.clip_fx(intensity, weights, thr=3, dsample_nfreq=512, dsample_nt=clipper_nt/16)
         rf_pipelines.clip_fx(intensity, weights, thr=3, axis=0, dsample_nt=clipper_nt)
