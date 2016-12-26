0d85f22240d5e2135e336c22fbc85511179021c2
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-26 11:53:42 -0500

    expand unit test (still doesn't quite pass)

--- test-kernels.cpp
+++ test-kernels.cpp
@@ -1,8 +1,9 @@
 // Note: this file should only include kernel headers (kernels/*.hpp), not toplevel headers (./*.hpp)
 // (This is because it has a Makefile dependency on the former but not the latter.)
 
-#include "kernels/clip2d.hpp"
+#include <cassert>
 #include "simd_helpers/simd_debug.hpp"
+#include "kernels/clip2d.hpp"
 
 using namespace std;
 using namespace rf_pipelines;
@@ -10,24 +11,10 @@ using namespace rf_pipelines;
 
 // -------------------------------------------------------------------------------------------------
 //
-// A few things cut-and-pasted from rf_pipelines_internals.hpp, since we don't want to depend on it.
+// General-purpose helpers
 
 
-#ifndef _unlikely
-#define _unlikely(cond)  (__builtin_expect(cond,0))
-#endif
-
-#define rf_assert(cond) rf_assert2(cond, __LINE__)
-
-#define rf_assert2(cond,line) \
-    do { \
-        if (_unlikely(!(cond))) { \
-	    const char *msg = "rf_pipelines: assertion '" __STRING(cond) "' failed (" __FILE__ ":" __STRING(line) ")\n"; \
-	    throw std::runtime_error(msg); \
-	} \
-    } while (0)
-
-
+// Cut-and-pasted from rf_pipelines_internals.hpp, since we don't want to #include it.
 template<typename T>
 inline T *aligned_alloc(size_t nelts)
 {
@@ -44,6 +31,20 @@ inline T *aligned_alloc(size_t nelts)
 }
 
 
+// Frequently used in conjunction with simd_helpers::vectorize()
+template<typename T> 
+inline double maxabs(const std::vector<T> &v)
+{
+    assert(v.size() > 0);
+
+    double ret = fabs(v[0]);
+    for (unsigned int i = 1; i < v.size(); i++)
+	ret = max(ret, fabs(double(v[i])));
+
+    return ret;
+}
+
+
 // -------------------------------------------------------------------------------------------------
 
 
@@ -55,6 +56,7 @@ struct random_chunk {
     float *intensity = nullptr;
     float *weights = nullptr;
 
+    random_chunk(std::mt19937 &rng, int nfreq, int nt, int stride);
     random_chunk(std::mt19937 &rng, int nfreq, int nt);
     ~random_chunk();
     
@@ -64,11 +66,12 @@ struct random_chunk {
 };
 
 
-random_chunk::random_chunk(std::mt19937 &rng, int nfreq_, int nt_)
-    : nfreq(nfreq_), nt(nt_), stride(nt + std::uniform_int_distribution<>(0,4)(rng))
+random_chunk::random_chunk(std::mt19937 &rng, int nfreq_, int nt_, int stride_) :
+    nfreq(nfreq_), nt(nt_), stride(stride_)
 {
-    rf_assert(nfreq > 0);
-    rf_assert(nt > 0);
+    assert(nfreq > 0);
+    assert(nt > 0);
+    assert(stride >= nt);
 
     intensity = aligned_alloc<float> (nfreq * stride);
     weights = aligned_alloc<float> (nfreq * stride);
@@ -80,6 +83,11 @@ random_chunk::random_chunk(std::mt19937 &rng, int nfreq_, int nt_)
 	weights[i] = std::uniform_real_distribution<>()(rng);
     }
 }
+
+
+random_chunk::random_chunk(std::mt19937 &rng, int nfreq_, int nt_) :
+    random_chunk(rng, nfreq_, nt_, nt_ + std::uniform_int_distribution<>(0,4)(rng))
+{ }
 
 
 random_chunk::~random_chunk()
@@ -96,8 +104,8 @@ random_chunk::~random_chunk()
 template<typename T>
 static void reference_clip2d_wrms(T &mean, T &rms, const T *intensity, const T *weights, int nfreq, int nt, int stride, int nds_f, int nds_t)
 {
-    rf_assert(nfreq % nds_f == 0);
-    rf_assert(nt % nds_t == 0);
+    assert(nfreq % nds_f == 0);
+    assert(nt % nds_t == 0);
 
     // double-precision here
     double acc0 = 0.0;
@@ -129,12 +137,13 @@ static void reference_clip2d_wrms(T &mean, T &rms, const T *intensity, const T *
 
 
 template<typename T, unsigned int S, unsigned int Df, unsigned int Dt>
-static void test_kernel_clip2d_wrms(std::mt19937 &rng)
+static void test_kernel_clip2d_wrms(std::mt19937 &rng, int nfreq, int nt, int stride)
 {
-    int nfreq = Df * std::uniform_int_distribution<>(10,20)(rng);
-    int nt = Dt * S * std::uniform_int_distribution<>(10,20)(rng);
+    assert(nfreq % Df == 0);
+    assert(nt % (Dt*S) == 0);
+    assert(stride >= nt);
 
-    random_chunk rc(rng, nfreq, nt);
+    random_chunk rc(rng, nfreq, nt, stride);
     
     simd_t<T,S> mean, rms;
     _kernel_clip2d_wrms<T,S,Df,Dt> (mean, rms, rc.intensity, rc.weights, nfreq, nt, rc.stride);
@@ -142,16 +151,65 @@ static void test_kernel_clip2d_wrms(std::mt19937 &rng)
     float ref_mean, ref_rms;
     reference_clip2d_wrms(ref_mean, ref_rms, rc.intensity, rc.weights, nfreq, nt, rc.stride, Df, Dt);
 
-    cout << ref_mean << " | " << mean << endl;
-    cout << ref_rms << " | " << rms << endl;
+    vector<float> delta1 = vectorize(mean - simd_t<T,S> (ref_mean));
+    vector<float> delta2 = vectorize(rms - simd_t<T,S> (ref_rms));    
+
+    assert(maxabs(delta1) < 1.0e-4 * Df*Dt);
+    assert(maxabs(delta2) < 1.0e-4 * sqrt(Df*Dt));
 }
+
+
+template<typename T, unsigned int S, unsigned int Df, unsigned int Dt>
+static void test_kernel_clip2d_wrms(std::mt19937 &rng)
+{
+    int nfreq = Df * std::uniform_int_distribution<>(10,20)(rng);
+    int nt = Dt * S * std::uniform_int_distribution<>(10,20)(rng);
+    int stride = nt + std::uniform_int_distribution<>(0,4)(rng);
+
+    test_kernel_clip2d_wrms<T,S,Df,Dt> (rng, nfreq, nt, stride);
+}
+
+
+// Fixed Df, many Dt
+template<typename T, unsigned int S, unsigned int Df, unsigned int MaxDt, typename std::enable_if<(MaxDt==1),int>::type = 0>
+static void test_kernel_clip2d_wrms_varying_dt(std::mt19937 &rng)
+{
+    test_kernel_clip2d_wrms<T,S,Df,1> (rng);
+}
+
+// Fixed Df, many Dt
+template<typename T, unsigned int S, unsigned int Df, unsigned int MaxDt, typename std::enable_if<(MaxDt>1),int>::type = 0>
+static void test_kernel_clip2d_wrms_varying_dt(std::mt19937 &rng)
+{
+    test_kernel_clip2d_wrms_varying_dt<T,S,Df,MaxDt/2> (rng);
+    test_kernel_clip2d_wrms<T,S,Df,MaxDt> (rng);
+}
+
+// Many Df, many Dt
+template<typename T, unsigned int S, unsigned int MaxDf, unsigned int MaxDt, typename std::enable_if<(MaxDf==1),int>::type = 0>
+static void test_kernel_clip2d_wrms_all(std::mt19937 &rng)
+{
+    test_kernel_clip2d_wrms_varying_dt<T,S,1,MaxDt> (rng);
+}
+
+// Many Df, many Dt
+template<typename T, unsigned int S, unsigned int MaxDf, unsigned int MaxDt, typename std::enable_if<(MaxDf>1),int>::type = 0>
+static void test_kernel_clip2d_wrms_all(std::mt19937 &rng)
+{
+    test_kernel_clip2d_wrms_all<T,S,MaxDf/2,MaxDt> (rng);
+    test_kernel_clip2d_wrms_varying_dt<T,S,MaxDf,MaxDt> (rng);
+}
+
+
+// -------------------------------------------------------------------------------------------------
 
 
 int main(int argc, char **argv)
 {
     std::random_device rd;
     std::mt19937 rng(rd());
-    
-    test_kernel_clip2d_wrms<float,8,1,1> (rng);
+
+    test_kernel_clip2d_wrms_all<float,8,32,32> (rng);
+
     return 0;
 }
