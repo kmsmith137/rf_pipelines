c481c7acbab7bfab177ef95a7dbb3acf61b6daab
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-26 11:36:43 -0500

    debug

--- kernels/clip2d.hpp
+++ kernels/clip2d.hpp
@@ -5,7 +5,6 @@
 #include "mean_rms_accumulator.hpp"
 #include "downsample.hpp"
 
-
 namespace rf_pipelines {
 #if 0
 }; // pacify emacs c-mode
@@ -36,8 +35,8 @@ void _kernel_clip2d_wrms(simd_t<T,S> &mean, simd_t<T,S> &rms, const T *intensity
     mean_rms_accumulator<T,S> acc;
 
     for (int ifreq = 0; ifreq < nfreq; ifreq += Df) {
-	const T *irow = intensity + Df*stride;
-	const T *wrow = weights + Df*stride;
+	const T *irow = intensity + ifreq*stride;
+	const T *wrow = weights + ifreq*stride;
 
 	for (int it = 0; it < nt; it += Dt*S) {
 	    simd_t<T,S> ival = _kernel_downsample<T,S,Df,Dt> (irow + it, stride);
