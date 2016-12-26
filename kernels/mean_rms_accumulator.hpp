d208952719b1f65388bc4d48295d9d782ce3f4cb
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-26 08:45:01 -0500

    mean_rms_accumulator::horizontal_sum()

--- kernels/mean_rms_accumulator.hpp
+++ kernels/mean_rms_accumulator.hpp
@@ -11,14 +11,16 @@ namespace rf_pipelines {
 
 // mean_rms_accumulator: helper class for computing weighted mean/rms (e.g. in clipper_transform)
 //
-// Each element of the simd_t<T,S> is processed independently.  
+// Each element of the simd_t<T,S> is processed independently.  If the desired behavior is to
+// sum them together instead, then call mean_rms_accumulator::horizontal_sum() before calling
+// mean_rms_accumulator::get_mean_rms() but after the calls to add().
 //
 // An entry is invalid if the mean and rms cannot be computed, either because the
 // sum of the weights is <= 0, or if the variance is too small compared to the mean.
 // Invalid entries are indicated by rms=0 and arbitrary mean.
 //
 // FIXME I think we should compute the mean/rms in double precision even when T=float.
-// Suggested generalization: struct mean_rms_accumulator<float, 8, double>
+// Suggested generalization: struct mean_rms_accumulator<float, 8, double>.
 
 
 template<typename T, unsigned int S>
@@ -33,6 +35,13 @@ struct mean_rms_accumulator {
 	acc0 += wval;
 	acc1 += wi;
 	acc2 += wi * ival;
+    }
+
+    inline void horizontal_sum()
+    {
+	acc0 = acc0.horizontal_sum();
+	acc1 = acc1.horizontal_sum();
+	acc2 = acc2.horizontal_sum();
     }
 
     inline void get_mean_rms(simd_t<T,S> &mean, simd_t<T,S> &rms) const
