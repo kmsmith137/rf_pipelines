22882f26160409f726bfab1ae5f38d167040e38a
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-01-05 13:22:19 -0500

    checked fast poly detrender (along time and freq): passed

--- rf_pipelines/transforms/simple_detrender.py
+++ rf_pipelines/transforms/simple_detrender.py
@@ -1,8 +1,9 @@
-# A "simple detrender" is a time-axis polynomial fitter with degree zero.
-# This file will be removed soon, in favor of calling make_polynomial_detrender_time_axis() directly.
-
 from rf_pipelines import rf_pipelines_c
 
-def simple_detrender(nt_detrend):
-    print >>sys.stderr, 'make_simple_detrender(): this function is now deprecated'
-    return rf_pipelines_c.make_polynomial_detrender_time_axis(nt_detrend, 0, 1.0e-2)
+def simple_detrender(nt_detrend, axis=1, deg=0, epsilon=1.0e-2):
+    if axis == 1:
+        return rf_pipelines_c.make_polynomial_detrender_time_axis(nt_detrend, deg, epsilon)
+    elif axis == 0:
+        return rf_pipelines_c.make_polynomial_detrender_freq_axis(nt_detrend, deg, epsilon)
+    else:
+        raise RuntimeError("axis must be 0 (along freq) or 1 (along time).")
