9fa191f47f6d241f52f359eeed6efb70e3919517
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-01-13 07:52:46 -0500

    std_dev_filter cleanup

--- rf_pipelines/transforms/std_dev_filter.py
+++ rf_pipelines/transforms/std_dev_filter.py
@@ -34,61 +34,74 @@ def filter_stdv(intensity, weights, thr=3, axis=1, dsample_nfreq=None, dsample_n
         # Downsample the weights and intensity.
         (intensity, weights) = rf_pipelines.wi_downsample(intensity, weights, dsample_nfreq, dsample_nt)
 
-    # Compute the weighted standard deviation of the intensity
-    # array along the selected axis.  We also build up a boolean
-    # mask 'sd_mask' which is False where 'sd' is invalid.
+    # Pass 1: Compute the weighted standard deviation of the intensity array along the 
+    # selected axis.  We also build up a weights array 'sd_weights' which is 0 or 1.
 
     if imitate_cpp:
         den = np.sum(weights, axis=axis)
-        sd_mask = (den > 0)
+        sd_weights = (den > 0)
 
         # In the C++ code, we use the variance rather than the standard deviation
         # (i.e. no square root), and subtract the mean.
-        den = np.where(sd_mask, den, 1.0)
+        den = np.where(sd_weights, den, 1.0)
         mean = np.sum(weights*intensity, axis=axis) / den
         mean_e = rf_pipelines.tile_arr(mean, axis, dsample_nfreq, dsample_nt)
         sd = np.sum(weights*(intensity-mean_e)**2, axis=axis) / den
 
         # Expand the mask to exclude elements where the standard deviation
         # is very small compared to the mean.
-        sd_mask = np.logical_and(sd_mask, (sd > (1.0e-10 * mean**2)))
+        sd_weights = np.logical_and(sd_weights, (sd > (1.0e-10 * mean**2)))
 
     else:
         num = np.asarray(np.sum(weights*(intensity)**2, axis=axis))
         den = np.asarray(np.sum(weights, axis=axis))
         np.putmask(den, den==0., 1.0)
         sd = np.sqrt(num/den)
-        sd_mask = np.ones_like(sd)
+        sd_weights = np.ones_like(sd)
 
-    # Tile 'sd' so that it matches with the shape of intensity.
-    sd = rf_pipelines.tile_arr(sd, axis, dsample_nfreq, dsample_nt)
-    sd_mask = rf_pipelines.tile_arr(sd_mask, axis, dsample_nfreq, dsample_nt)
+    # Pass 2: Compute the mean and rms of the 'sd' array, and clip based on it.
+    # The result is a 2D boolean array 'mask' (clipped values are represented by True).
 
-    # This block of code creates a mask, and hence filters,
-    # based on the mean and stdv of 'sd' along THE OTHER axis.
-    axis = np.abs(axis-1)
-    den = np.sum(sd_mask, axis=axis)
+    if imitate_cpp:
 
-    # Avoid divide-by-zero [explain]
-    den = np.maximum(den, 1)
+        # Code cleanup: compute the mean/rms by calling weighted_mean_and_rms().
+        #
+        # Note: it would be trivial to implement iterated clipping, since
+        # weighted_mean_and_rms() already has a 'niter' argument.  If this
+        # turns out to be useful, then it would have tiny computational cost,
+        # since the iteration is done when the array is 1D!
+        
+        (sd_mean, sd_rms) = rf_pipelines.weighted_mean_and_rms(sd, sd_weights, sigma_clip=thr)
 
-    sd_mean0 = np.sum(sd_mask * sd, axis=axis) / den
-    sd_mean = rf_pipelines.tile_arr(sd_mean0, axis, dsample_nfreq, dsample_nt)
+        # 1D boolean mask (clipped values are represented by True)
+        mask = (np.abs(sd-sd_mean) >= thr*sd_rms)
 
-    # Regulate [explain]
-    sd_var = np.sum(sd_mask * (sd-sd_mean)**2, axis=axis) / den
-    sd_var = np.where(sd_var > 1.0e-10 * sd_mean0**2, sd_var, 0.0)
-    sd_stdv = rf_pipelines.tile_arr(sd_var**0.5, axis, dsample_nfreq, dsample_nt)
+        # 2D boolean mask (still needs upsampling)
+        mask = rf_pipelines.tile_arr(mask, axis, dsample_nfreq, dsample_nt)
 
-    assert sd.shape == sd_mean.shape == sd_stdv.shape
+    else:
+        # Tile 'sd' so that it matches with the shape of intensity.
+        sd = rf_pipelines.tile_arr(sd, axis, dsample_nfreq, dsample_nt)
 
-    # Boolean array which is True for masked values
-    mask = np.abs(sd-sd_mean) >= (thr*sd_stdv)
+        # This block of code creates a mask, and hence filters, 
+        # based on the mean and stdv of 'sd' along THE OTHER axis.
+        axis = np.abs(axis-1)
+        sd_stdv = rf_pipelines.tile_arr(sd.std(axis=axis), axis, dsample_nfreq, dsample_nt)
+        sd_mean = rf_pipelines.tile_arr(sd.mean(axis=axis), axis, dsample_nfreq, dsample_nt)
 
+        assert sd.shape == sd_mean.shape == sd_stdv.shape
+
+        # Boolean array which is True for masked values
+        mask = np.abs(sd-sd_mean) > (thr*sd_stdv)
+        
+
+    # Upsample to original resolution
     if coarse_grained:
         mask = rf_pipelines.upsample(mask, nfreq, nt_chunk)
 
+    # Apply mask to original hi-res weights array
     np.putmask(weights_hres, mask, 0.)
+
 
 class std_dev_filter(rf_pipelines.py_wi_transform):
     """
