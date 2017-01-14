53f1fc53eeba5048c9ab155c8a1a8c5604a1710a
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-01-13 23:28:44 -0500

    synch with new changes in cpp

--- rf_pipelines/transforms/intensity_clipper_cpp.py
+++ rf_pipelines/transforms/intensity_clipper_cpp.py
@@ -6,7 +6,7 @@ See FIXME intensity_clippers.cpp, and python linkage in rf_pipelines_c.cpp.
 import numpy as np
 from rf_pipelines import rf_pipelines_c
 
-def intensity_clipper_cpp(axis=None, Df=1, Dt=1, nt_chunk=1024, sigma=3, niter=1, iter_sigma=3):
+def intensity_clipper_cpp(nt_chunk=1024, axis=None, sigma=3, niter=1, iter_sigma=3, Df=1, Dt=1):
 
     """
     Returns a transform object (wi_transform) which uses the intensity
@@ -15,17 +15,15 @@ def intensity_clipper_cpp(axis=None, Df=1, Dt=1, nt_chunk=1024, sigma=3, niter=1
 
     Constructor syntax:
 
-      t = intensity_clipper_cpp(axis=None, Df=1, Dt=1, nt_chunk=1024, sigma=3, niter=1, iter_sigma=3)
+      t = intensity_clipper_cpp(nt_chunk=1024, axis=None, sigma=3, niter=1, iter_sigma=3, Df=1, Dt=1)
       
+      'nt_chunk=1024' is the chunk size (in number of samples).
+
       'axis=None' is the axis convention:
         None: planar; freq and time. 
         0: along freq; constant time.
         1: along time; constant freq.
  
-      'Df=1' and 'Dt=1' are the frequency and time downsampling factors, respectively.
-
-      'nt_chunk=1024' is the chunk size (in number of samples).
-
       'sigma=3' is the threshold (in sigmas from the mean) for clipping. 
         Note: The weights are used when calculating both the mean and rms intensity.
 
@@ -34,16 +32,9 @@ def intensity_clipper_cpp(axis=None, Df=1, Dt=1, nt_chunk=1024, sigma=3, niter=1
       'iter_sigma=3' is the threshold value for clipping in internal iterations.
         Note: This value need not be the same as 'sigma'
 
-    """
-    assert (axis == None or axis == 0 or axis == 1), "axis must be None (planar; freq and time), 0 (along freq; constant time), or 1 (along time; constant freq)."
-    assert ((np.log2(Df) % 2) in (0., 1.)) and ((np.log2(Dt) % 2) in (0., 1.)), "Downsampling factors must be powers of 2"
-    assert nt_chunk > 0
-    assert (sigma >= 1. and iter_sigma >= 1.), "threshold values must be >= 1."
-    assert niter >= 1
+      'Df=1' and 'Dt=1' are the frequency and time downsampling factors, respectively.
 
-    if axis == 1:
-        return TODO
-    elif axis == 0:
-        return TODO
-    else:
-        return rf_pipelines_c.make_intensity_clipper2d(Df, Dt, nt_chunk, sigma, niter, iter_sigma)
+    """
+    assert ((np.log2(Df) % 2) in (0., 1.)) and ((np.log2(Dt) % 2) in (0., 1.)), "Downsampling factors must be powers of 2"
+    
+    return rf_pipelines_c.make_intensity_clipper(nt_chunk, axis, sigma, niter, iter_sigma, Df, Dt)
--- rf_pipelines/transforms/polynomial_detrender_cpp.py
+++ rf_pipelines/transforms/polynomial_detrender_cpp.py
@@ -5,32 +5,27 @@ See polynomial_detrenders.cpp, and python linkage in rf_pipelines_c.cpp.
 
 from rf_pipelines import rf_pipelines_c
 
-def polynomial_detrender_cpp(nt_detrend, axis=1, deg=0, epsilon=1.0e-2):
+def polynomial_detrender_cpp(nt_chunk, axis=1, polydeg=0, epsilon=1.0e-2):
     """
     Returns a transform object (wi_transform) which detrends the intensity
     along time or frequency.
 
     Constructor syntax:
 
-      t = polynomial_detrender_cpp(nt_detrend, axis=1, deg=0, epsilon=1.0e-2)
+      t = polynomial_detrender_cpp(nt_chunk, axis=1, polydeg=0, epsilon=1.0e-2)
       
-      'nt_detrend=1024' is the chunk size (in number of samples).
+      'nt_chunk=1024' is the chunk size (in number of samples).
 
       'axis=1' is the axis convention:
         0: along freq; constant time.
         1: along time; constant freq.
 
-      'deg=0' is the degree of fit.
+      'polydeg=0' is the degree of fit.
 
       'epsilon=1.0e-2' is the threshold value for a poorly conditioned fit.
 
     """ 
-    assert (deg >= 0 and type(deg) == int), "degree must be an integer >= 0"
+    assert (polydeg >= 0 and type(polydeg) == int), "poly degree must be an integer >= 0"
     assert epsilon > 0., "choose a threshold value > 0 or leave it as default (1.0e-2)"
 
-    if axis == 1:
-        return rf_pipelines_c.make_polynomial_detrender_time_axis(nt_detrend, deg, epsilon)
-    elif axis == 0:
-        return rf_pipelines_c.make_polynomial_detrender_freq_axis(nt_detrend, deg, epsilon)
-    else:
-        raise RuntimeError("axis must be 0 (along freq) or 1 (along time).")
+    return rf_pipelines_c.make_polynomial_detrender(nt_chunk, axis, polydeg, epsilon)
