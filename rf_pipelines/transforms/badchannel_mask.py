75d28d31187629db78f94b749752c95f15637961
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-02-05 18:12:24 -0500

    modify default arg values

--- rf_pipelines/transforms/badchannel_mask.py
+++ rf_pipelines/transforms/badchannel_mask.py
@@ -7,7 +7,7 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
 
     Constructor syntax:
 
-      t = badchannel_mask(maskpath, nt_chunk=1024, mask)
+      t = badchannel_mask(maskpath=None, nt_chunk=1024, mask=None)
 
       'maskpath' is the full path to a mask file that contains affected freq 
        intervals, written in rows with the following format: e.g., 420.02,423.03
@@ -18,7 +18,7 @@ class badchannel_mask(rf_pipelines.py_wi_transform):
       'mask' is a list of user-supplied freq pairs for (additional) masking.
     """
 
-    def __init__(self, maskpath, nt_chunk=1024, mask=None):
+    def __init__(self, maskpath=None, nt_chunk=1024, mask=None):
         
         self.maskpath = maskpath 
         self.name = 'badchannel_mask(%s)' % maskpath
