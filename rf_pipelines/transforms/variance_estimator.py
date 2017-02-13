384bfd291067e0131970db7f83585c3f34c444d8
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-02-13 15:24:08 -0500

    Basic implementation of variance estimator; no interpolation

--- rf_pipelines/transforms/variance_estimator.py
+++ rf_pipelines/transforms/variance_estimator.py
@@ -1,23 +1,24 @@
 import sys
 import rf_pipelines
 import numpy as np
-np.set_printoptions(threshold=np.nan)
-# import matplotlib.pyplot as plt
 
-# First implementation - plot v_1 over time interval on long run and plot! 
-# Good way to test chunk assumption! 
+# HEY FORGETFUL MAYA: make sure you add interpolation at some point! 
+
+
+np.set_printoptions(threshold=np.nan)
 
 
 class variance_estimator(rf_pipelines.py_wi_transform):
     """
     This is a pseudo-transform (meaning that it does not actually modify its input). 
 
-    For now, compute v_1 with short timesamples and plot!
-
+    Current strategy: calculate variance of v1_chunk number of samples return the median of 
+    v2_chunk number of v1 values. 
+    
     Maybe introduce a frequency downsample parameter later on?
     """
 
-    def __init__(self, v1_chunk=64, nt_chunk=1024):
+    def __init__(self, v1_chunk=64, v2_chunk = 32, nt_chunk=1024):
         name = "variance_estimator('v1_chunk=%d, nt_chunk=%d')" % (v1_chunk, nt_chunk)
 
         # Call base class constructor
@@ -27,6 +28,7 @@ class variance_estimator(rf_pipelines.py_wi_transform):
             'For now, nt_chunk(=%d) must be a multiple of v1_chunk(=%d)' % (nt_chunk, v1_chunk)
         
         self.v1_chunk = v1_chunk
+        self.v2_chunk = v2_chunk
         self.nt_chunk = nt_chunk
         self.nt_prepad = 0   # might want to do something with this later
         self.nt_postpad = 0  # but for now, assume nt_chunk is all we are looking at (must be multiple of v1_samples
@@ -43,29 +45,37 @@ class variance_estimator(rf_pipelines.py_wi_transform):
 
     def start_substream(self, isubstream, t0):
         # Called once per substream (a stream can be split into multiple substreams).
-        self.v1 = []
-
+        self.v1 = np.zeros((self.nfreq, self.v1_chunk))
+        self.iv1 = np.zeros((self.nfreq), dtype=np.int32) # keeps track of which positon we are adding v1 to 
+        self.v2 = []
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
         # This is the main computational routine defining the transform, which is called
         # once per incoming "block" of data.  For documentation on the interface see
         # rf_pipelines.py_wi_transform docstring.
 
+        # For testing - we are only viewing 4 frequencies
         for frequency in xrange(self.nfreq-1000-20):
+            if frequency == 0:
+                print intensity[0]
+                print weights[0]
             for i in xrange(0, len(intensity), self.v1_chunk):
                 a = self._v1(intensity[frequency, i : i+self.v1_chunk], weights[frequency, i : i+self.v1_chunk])
-                self.v1.append(a)
-                print '*' * 10
-                if a > 10:
-                    print a
-                    print intensity[frequency, i : i+self.v1_chunk]
-                    print weights[frequency, i : i+self.v1_chunk]
-
-
+                self.v1[frequency, self.iv1[frequency]] = a
+                self.iv1[frequency] += 1
+                if self.iv1[frequency] == self.v2_chunk:
+                    if np.isnan(self.v1[frequency]).sum() > self.v2_chunk * 0.75:
+                        print "** NAN **"
+                        self.v2.append(np.nan)
+                    else:
+                        print 'med for', frequency, np.median(self.v1[frequency])
+                        self.v2.append(np.median(self.v1[frequency]))
+                    self.v1[frequency] = []
+                    self.iv1[frequency] = 0
 
     def end_substream(self):
-        # Reshape self.v1
-        out = np.array(self.v1).reshape((self.nfreq-1000-20, -1))
+        # Reshape self.v2
+        out = np.array(self.v2).reshape((self.nfreq-1000-20, -1))
         print '-' * 80
         print out.shape
         print out
@@ -75,8 +85,9 @@ class variance_estimator(rf_pipelines.py_wi_transform):
 
     def _v1(self, i, w):
         w_sum = w.sum()
-        if w_sum == 0: 
-            return None
+        if np.count_nonzero(w == 0) > self.v1_chunk * 0.75:
+            print "*** NAN ***"
+            return np.nan
         average = np.average(i, weights=w)
         variance = np.average((i-average)**2, weights=w)  # Fast and numerically precise
         return variance
