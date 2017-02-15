71cc47dd35b870d9cfb7de14bb136a4f34d9b8a6
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-02-15 14:19:33 -0500

    Fix v1 loop bug, update invalid convention from nan to 0, add fname constructor argument

--- rf_pipelines/transforms/variance_estimator.py
+++ rf_pipelines/transforms/variance_estimator.py
@@ -15,9 +15,10 @@ class variance_estimator(rf_pipelines.py_wi_transform):
         (nfreq, total number of time samples / v1_chunk / v1_chunk)
 
     The variance array is written out as a .npy file in the directory of the test script.
+    The prefix for this outputted file can be determined by the fname argument. 
     """
 
-    def __init__(self, v1_chunk=128, v2_chunk=64, nt_chunk=1024):
+    def __init__(self, v1_chunk=128, v2_chunk=64, nt_chunk=1024, fname=None):
         name = "variance_estimator('v1_chunk=%d, v2_chunk=%d, nt_chunk=%d')" % (v1_chunk, v2_chunk, nt_chunk)
 
         # Call base class constructor
@@ -31,6 +32,7 @@ class variance_estimator(rf_pipelines.py_wi_transform):
         self.nt_chunk = nt_chunk
         self.nt_prepad = 0  
         self.nt_postpad = 0 
+        self.fname = fname
         
 
     def set_stream(self, s):
@@ -42,7 +44,7 @@ class variance_estimator(rf_pipelines.py_wi_transform):
     def start_substream(self, isubstream, t0):
         # Called once per substream (a stream can be split into multiple substreams).
         self.v1 = np.zeros((self.nfreq, self.v1_chunk))
-        self.iv1 = np.zeros((self.nfreq), dtype=np.int32) # keeps track of which positon we are adding v1 to 
+        self.iv1 = np.zeros((self.nfreq), dtype=np.int32)  # keeps track of which positon we are adding v1 to 
         self.v2 = []
 
     def process_chunk(self, t0, t1, intensity, weights, pp_intensity, pp_weights):
@@ -50,17 +52,19 @@ class variance_estimator(rf_pipelines.py_wi_transform):
         # once per incoming "block" of data.  For documentation on the interface see
         # rf_pipelines.py_wi_transform docstring.
         for frequency in xrange(self.nfreq):
-            for i in xrange(0, len(intensity), self.v1_chunk):
+            for i in xrange(0, self.nt_chunk, self.v1_chunk):
                 # Compute v1 for each frequency
                 a = self._v1(intensity[frequency, i : i+self.v1_chunk], weights[frequency, i : i+self.v1_chunk])
                 self.v1[frequency, self.iv1[frequency]] = a
                 self.iv1[frequency] += 1
                 if self.iv1[frequency] == self.v2_chunk:
-                    # Empty v1[frequency] and compute median for v2 (nan if too many v1 values are nan)
-                    if np.isnan(self.v1[frequency]).sum() > self.v2_chunk * 0.75:
-                        self.v2.append(np.nan)
+                    # Empty v1[frequency] and compute median for v2 (0 if too many v1 values are 0)
+                    if np.count_nonzero(self.v1[frequency]) < self.v2_chunk * 0.25: 
+                        self.v2.append(0)
                     else:
-                        self.v2.append(np.median(self.v1[frequency]))
+                        # Here, we want to ignore elements with value 0
+                        # print np.median(self.v1[frequency][np.nonzero(self.v1[frequency])])
+                        self.v2.append(np.median(self.v1[frequency][np.nonzero(self.v1[frequency])]))
                     self.v1[frequency] = []
                     self.iv1[frequency] = 0
 
@@ -69,29 +73,28 @@ class variance_estimator(rf_pipelines.py_wi_transform):
         # Reshape self.v2
         out = np.array(self.v2).reshape((self.nfreq, -1))
 
-        # Interpolate nans
+        # Interpolate zeros
         indices = np.arange(len(out[0]))
         for frequency in xrange(self.nfreq):
-            not_nan = np.logical_not(np.isnan(out[frequency]))
-            if True not in not_nan:
+            nonzero = np.nonzero(out[frequency])[0]
+            if len(nonzero) == 0:
                 out[frequency] = np.zeros((len(out[0])))
             else:
-                out[frequency] = np.interp(indices, indices[not_nan], out[frequency, not_nan])
-
+                out[frequency] = np.interp(indices, nonzero, out[frequency, nonzero])
+    
         # Write data to script directory
-        np.save('var-%s.npy' % (time.strftime('%y-%m-%d-%X')), out)
+        if self.fname is None:
+            np.save('var-%s.npy' % (time.strftime('%y-%m-%d-%X')), out)
+        else:
+            np.save('%s-%s.npy' % (self.fname, time.strftime('%y-%m-%d-%X')), out)
 
 
     def _v1(self, i, w):
         """Calculate weighted variance"""
         w_sum = w.sum()
-        if np.count_nonzero(w == 0) > self.v1_chunk * 0.75:
-            return np.nan
+        if np.count_nonzero(w) < self.v1_chunk * 0.25:
+            return 0
         average = np.average(i, weights=w)
         variance = np.average((i-average)**2, weights=w) 
         return variance
-        
-
-    def _nan_helper(self, a):
-        """Input 1D array"""
-        return np.isnan(a), lambda z: z.nonzero()[0]
+    
