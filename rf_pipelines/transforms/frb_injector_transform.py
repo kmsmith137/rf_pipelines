2197461c7f3316846e2a7a4001822173a107f5f7
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-02-16 12:13:27 +0200

    add comments to frb_injector_transform.py

--- rf_pipelines/transforms/frb_injector_transform.py
+++ rf_pipelines/transforms/frb_injector_transform.py
@@ -2,7 +2,50 @@ import sys
 import rf_pipelines
 
 class frb_injector_transform(rf_pipelines.py_wi_transform):
+    """
+    This transform modifies the intensities in the pipeline by adding a simulated FRB
+    (leaving weights unmodified).  For example, it can be used to add a simulated FRB
+    to a "replayed" pathfinder acquisition which includes RFI.
+
+    WARNING: the transform currently includes a 'snr' parameter which is a placeholder
+    for the signal-to-noise ratio in "sigmas".  However, this is currently implemented 
+    assuming that the noise variance is 1.0 in every channel!  This is a bad approximation,
+    and so the true signal-to-noise ratio of the simulated pulse will be very different
+    from the nominal snr specified in the constructor.  Now that variance estimation
+    has been implemented in the pipeline, this can be fixed.
+    """
+
     def __init__(self, snr, undispersed_arrival_time, dm, intrinsic_width=0.0, sm=0.0, spectral_index=0.0, sample_rms=1.0, nt_chunk=1024):
+        """
+        Constructor arguments
+        ---------------------
+
+           snr: Signal-to-noise ratio in "sigmas".  Currently a placeholder and not implemented correctly!
+                (See note in the class docstring.)
+
+           undispersed_arrival_time: Arrival time (in seconds) of the pulse without dispersion delay applied.
+                This can be compared to other timestamps in the pipeline (e.g. the t0,t1 arguments to
+                process_triggers().)
+
+           dm: Dispersion measure in its usual units (pc cm^{-3})
+
+           intrinsic_width: Width of the pulse in seconds, frequency-independent Gaussian profile assumed.
+             Note that even a pulse with intrinsic_width=0 will have nonzero width in the channelized data, 
+             due to dispersion smearing within a frequency channel.
+
+           sm: The scattering measure, which we define to be the scattering timescale in milliseconds (not seconds!)
+             at 1 GHz.  Note that 'scattering' refers to scatter-broadening of the pulse due to ISM turbulence, and
+             is assumed to have an exponentially decaying profile whose timescale is proportional to frequency^(-4.4).
+             (This can be compared to intrinsic_width, which has a Gaussian profile and no frequency dependence.)
+
+           spectral_index: The spectral index beta is defined by the statement that the strength of the pulse
+             is proportional to frequency^beta.
+
+           sample_rms: The square root of the sample variance.  Currently this is assumed to be the same for
+             all channels, and we have no good way of estimating its value.  This can be improved, now that
+             variance estimation is implemented in the pipeline!
+        """
+
         self.dm = dm
         self.sm = sm
         self.snr = snr
@@ -22,15 +65,18 @@ class frb_injector_transform(rf_pipelines.py_wi_transform):
         try:
             import simpulse
         except ImportError:
-            # Print an installation hint...
-            print >>sys.stderr, "\n*** Note: If you don't have the 'simpulse' library, it can be downloaded at https://github.com/kmsmith137/simpulse ***\n"
-            # ...then print an error message and exit
-            raise
+            raise RuntimeError("rf_pipelines: couldn't import the 'bonsai' module.  You may need to clone https://github.com/kmsmith137/simpulse and install.")
 
         self.nfreq = stream.nfreq
         self.freq_lo_MHz = stream.freq_lo_MHz
         self.freq_hi_MHz = stream.freq_hi_MHz
         self.dt_sample = stream.dt_sample
+
+        # Construct a simpulse.single_pulse object, which represents a single FRB signal, to be added to the timestream in chunks.
+        #
+        # The mechanism implemented in simpulse for obtaining a pulse with specified signal-to-noise ratio is a little awkward!
+        # First we construct the pulse with an arbitrary normalization, then we compute the signal-to-noise of the pulse given
+        # its arbitrary normalization, then we adjust the normalization accordingly.
 
         self.pulse = simpulse.single_pulse(1024,       # number of samples used "under the hood" in the simpulse library
                                            self.nfreq,
@@ -43,10 +89,31 @@ class frb_injector_transform(rf_pipelines.py_wi_transform):
                                            self.spectral_index,
                                            self.undispersed_arrival_time)
 
-        # signal-to-noise with fluence=1
-        snr0 = self.pulse.get_signal_to_noise(self.dt_sample, 0.0, self.sample_rms)
+        # Compute the signal-to-noise ratio of the pulse, for the arbitrary normalization specified in the constructor.
+        #
+        # This is the part that needs to be modified to include variance estimation!  If you look at the docstring,
+        # you'll see that simpulse.single_pulse.get_signal_to_noise() has a 'sample_rms' argument and a 'channel_weights' 
+        # argument.  (Note: the "rms" or "root-mean-square" is defined to be the square root of the variance.)
+        #
+        # Currently, we take sample_rms to be a scalar, which simpulse interprets as meaning that all frequency
+        # channels have the same rms.  More generally, if sample_rms is a 1D numpy array of length nfreq, then
+        # simpulse interprets it as a per-frequency rms.
+        #
+        # If the rms is frequency-dependent, then the weighting of the channels is also important when determining
+        # the signal-to-noise.  This is specified via the 'channel_weights' argument, also an array of length nfreq,
+        # to simpulse.single_pulse.get_signal_to_noise().  The best thing to use here would be a per-frequency estimate 
+        # of the average value of the pipeline 'weights' array, but I propose just taking the weights to be 0.0 in
+        # bad frequency channels (defined as channels with many v2 failures during variance estimation), and 1.0 in
+        # non-bad channels.  Note that leaving channel_weights unspecified (or equivalently setting channel_weights=None)
+        # defaults to 1/sigma_rms^2 weighting, which is inconsistent with what we're currently doing in the pipeline.
+        #
+        # (BTW the general issue of optimal frequency channel weighting is a nontrivial one that we'll want to revisit,
+        # so the channel-weighting logic in the previous paragraph will probably be changed later!)
         
-        # adjust fluence of pulse to target S/N
+        snr0 = self.pulse.get_signal_to_noise(self.dt_sample, self.sample_rms)
+        
+        # The pulse has been simulated with nominal fluence 1, and its computed signal-to-noise is 'snr0'.
+        # We want to end up with signal-to-noise 'snr'.  Therefore, the following fluence gives the signal-to-noise we want.
         self.pulse.fluence = self.snr / snr0
 
     
