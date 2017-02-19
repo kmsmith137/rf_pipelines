d7b9878b4dbd9701251b3f3088460e6280012bf4
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-02-18 20:49:17 -0500

    Add extra info to variance estimator out fname as it is quite helpful for mask_filler

--- rf_pipelines/transforms/variance_estimator.py
+++ rf_pipelines/transforms/variance_estimator.py
@@ -86,7 +86,7 @@ class variance_estimator(rf_pipelines.py_wi_transform):
     
         # Write data to script directory
         if self.fname is None:
-            np.save('var-%s.npy' % (time.strftime('%y-%m-%d-%X')), out)
+            np.save('var-v1%d-v2%d-%s.npy' % (self.v1_chunk, self.v2_chunk, time.strftime('%y-%m-%d-%X')), out)
         else:
             np.save('%s-%s.npy' % (self.fname, time.strftime('%y-%m-%d-%X')), out)
 
