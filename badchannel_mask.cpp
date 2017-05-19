1f39600ab5cfe90f41d095e799da757a24018e13
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-05-19 10:19:19 -0400

    Clean up and fix bug in process_chunk

--- badchannel_mask.cpp
+++ badchannel_mask.cpp
@@ -10,44 +10,34 @@ namespace rf_pipelines {
 #endif
 
 
-// This file is a placeholder for a C++ implementation of the 'badchannel_mask' class.
-//
-// Right now, it just contains some general boilerplate needed to make a C++ badchannel_mask
-// class available throughout the pipeline.  In particular, the C++ badchannel_mask can be
-// obtained from python as 'rf_pipelines.rf_pipelines_c.make_badchannel_mask()'.
-//
-// The actual implementation of the badchannel_mask class is still missing -- right now,
-// all member functions throw exceptions.
-
 struct badchannel_mask : public wi_transform {
     // Note: inherits { nfreq, nt_chunk, nt_prepad, nt_postpad } from base class wi_transform
 
-    vector<float> bad_channels; // holds the bad frequencies, as specified from the input file
-    vector<int> bad_indices; // holds the final bad indices to be used to mask the weights array
-    int len_indices; // the size of bad_indices
+    vector<float> m_bad_channels;  // holds the bad frequencies, as specified from the input file
+    vector<int> m_bad_indices;     // holds the final bad indices to be used to mask the weights array
+    int m_len_indices;             // the size of bad_indices vector
    
     badchannel_mask(const string &maskpath, int nt_chunk)
     {
         stringstream ss;
 	ss << "badchannel_mask_cpp(maskpath=" << maskpath << ", nt_chunk=" << nt_chunk << ")";
         this->name = ss.str();
-
-        // Extract the channels to be removed into bad_channels
-	this->_get_bad_channels(maskpath, this->bad_channels);
 	this->nt_prepad = 0;
 	this->nt_postpad = 0;
 	this->nt_chunk = nt_chunk;
+
+        // Extract the channels to be removed into m_bad_channels
+	get_bad_channels(maskpath, m_bad_channels);
     }
 
-    void _get_bad_channels(const string &maskpath, vector<float> &bad_channels)
+    void get_bad_channels(const string &maskpath, vector<float> &bad_channels)
     {
         ifstream inf(maskpath);
-        string line;
-        string freq;
+        string line, freq;
         int length = 0;
         float low, high;
 
-        while (getline(inf,line))
+        while (getline(inf, line))
         {
             stringstream linestream(line);
 
@@ -73,8 +63,8 @@ struct badchannel_mask : public wi_transform {
 
         inf.close();
     
-        cout << "Frequency pairs extracted: " << endl;
-        for (int i = 0; i < length; i += 2) cout << bad_channels[i] << " " << bad_channels[i + 1] << endl;
+        // cout << "Frequency pairs extracted: " << endl;
+        // for (int i = 0; i < length; i += 2) cout << bad_channels[i] << " " << bad_channels[i + 1] << endl;
     }
 
     // As explaned in rf_pipelines.hpp, the following four virtual functions in the base class
@@ -87,13 +77,13 @@ struct badchannel_mask : public wi_transform {
 	float freq_hi_MHz = stream.freq_hi_MHz;
 
 	// First, make sure all intervals are within the freq
-	int vec_size = this->bad_channels.size();
+	int vec_size = m_bad_channels.size();
 	float low, high;
 	vector<float> temp;
 	for (int ilo=0; ilo < vec_size-1; ilo+=2)
         {
-	    low = this->bad_channels[ilo];
-	    high = this->bad_channels[ilo + 1];
+	    low = m_bad_channels[ilo];
+	    high = m_bad_channels[ilo + 1];
 	    // Both ends of the range are within the observation range
 	    if (low > freq_lo_MHz && high < freq_hi_MHz)
 	    {
@@ -128,30 +118,33 @@ struct badchannel_mask : public wi_transform {
 	// We take the max at the end to ensure no values are below 0.
 	float scale = stream.nfreq / (freq_hi_MHz - freq_lo_MHz);
 	float factor = scale * freq_hi_MHz;
-	this->len_indices = temp.size();
-	for (int i = 0; i < this->len_indices; ++i)
+	m_len_indices = temp.size();
+	for (int i = 0; i < m_len_indices; ++i)
 	{
 	    if (i % 2 == 0)
-	        this->bad_indices.push_back(max(int(ceil(factor - temp[i]*scale)), 0));
+	        m_bad_indices.push_back(max(int(ceil(factor - temp[i]*scale)), 0));
 	    else
-	        this->bad_indices.push_back(max(int(floor(factor - temp[i]*scale)), 0));
+	        m_bad_indices.push_back(max(int(floor(factor - temp[i]*scale)), 0));
 	}
-	//	cout << "FINAL INDICES " << endl;
-        //      for (int i = 0; i < this->len_indices; i += 2) cout << bad_indices[i] << " " << bad_indices[i + 1] << endl;
+	// cout << "Indices extracted: " << endl;
+        // for (int i = 0; i < this->len_indices; i += 2) cout << bad_indices[i] << " " << bad_indices[i + 1] << endl;
     }
 
     virtual  void start_substream(int isubstream, double t0) override
     {
-        // Stuff here
+        // Nothing necessary here
     }
 
     virtual void process_chunk(double t0, double t1, float *intensity, float *weights, ssize_t stride, float *pp_intensity, float *pp_weights, ssize_t pp_stride) override
     {
-        for (int ibad_index=0; ibad_index < this->len_indices; ibad_index+=2)
+         // Loop over bad indices list
+        for (int ibad_index=0; ibad_index < m_len_indices; ibad_index+=2)
         {
-	    for (int ifreq=this->bad_indices[ibad_index+1]; ifreq <= this->bad_indices[ibad_index]; ++ifreq)
+	    // Iterate over each frequency in the bad index range
+	    for (int ifreq=m_bad_indices[ibad_index+1]; ifreq <= m_bad_indices[ibad_index]; ++ifreq)
 	    {
-	        for (int it=0; it < stride; ++it)
+	        // Set all weights in the channel to 0
+	        for (int it=0; it < nt_chunk; ++it)
 		    weights[ifreq*stride+it] = 0;
 	    }
         }
@@ -159,7 +152,7 @@ struct badchannel_mask : public wi_transform {
 
     virtual void end_substream() override
     {
-        // Stuff here
+        // Nothing necessary here
     }
 };
 
