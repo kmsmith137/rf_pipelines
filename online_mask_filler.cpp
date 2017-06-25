6f33695d28396972c36d536f4906791083866541
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-06-25 17:02:25 -0400

    Intermediate commit

--- online_mask_filler.cpp
+++ online_mask_filler.cpp
@@ -1,8 +1,8 @@
 #include "rf_pipelines_internals.hpp"
-#include <algorithm> // for max/min
-#include <cmath> // for sqrt
-#include <random> // for random_device, mt_19937, and random_distribution
 #include <sys/time.h>
+#include <random>  // for random_device
+#include "emmintrin.h" // for xor intrinsic
+#include "immintrin.h" // for the rest of the intrinsics
 
 using namespace std;
 
@@ -11,49 +11,42 @@ namespace rf_pipelines {
 };  // pacify emacs c-mode
 #endif
 
-
 // -------------------------------------------------------------------------------------------------
-//
-// online_mask_filler class
-//
-// Note: the online_mask_filler class declaration, and definitions of member functions
-// are local to this file, but at the end we define the externally-visible factory
-// function make_online_mask_filler(), which returns a pointer to a new online_mask_filler
-// object.
-//
-// Recommended reading: the declaration of 'struct wi_transform' in rf_pipelines.hpp
-// and comments contained therein.  This will explain (I hope!) what needs to be implemented,
-// for example in online_mask_filler::process_chunk().
-
-struct xorshift_plus {
-  // Initial seed given by arbitrary hardcoded constants.
-  // This is fine for timing, but should come from a std::random_device in production.
-  uint64_t s0 = 3289321;
-  uint64_t s1 = 4328934;
-
-  // Returns a random integer in the range (0, 2^64-1).
-  inline uint64_t gen_u64()
+// Class for random number generation
+// Generates eight random 32-bit floats using a vectorized implementation of xorshift+
+// between (-1, 1)
+struct vec_xorshift_plus {
+  random_device rd;
+  __m256i s0 = _mm256_setr_epi64x(rd(), rd(), rd(), rd());
+  __m256i s1 = _mm256_setr_epi64x(rd(), rd(), rd(), rd());
+  
+  inline __m256 gen_floats()
   {
-    uint64_t x = s0;
-    uint64_t y = s1;
+    // x = s0
+    __m256i x = s0;
+    // y = s1
+    __m256i y = s1;
+    // s0 = y
     s0 = y;
-    x ^= (x << 23);
-    s1 = x ^ y ^ (x >> 17) ^ (y >> 26);
-    return s1 + y;
-  }
-
-  // Returns a random float in the range (0,1).
-  inline float gen_float()
-  {
-    // The prefactor here is 2^(-64).
-    return 5.421010862e-20f * float(gen_u64());
+    // x ^= (x << 23)
+    x = _mm256_xor_si256(x, _mm256_slli_epi64(x, 23));
+    // s1 = x ^ y ^ (x >> 17) ^ (y >> 26)
+    s1 = _mm256_xor_si256(x, y);
+    s1 = _mm256_xor_si256(s1, _mm256_srli_epi64(x, 17));
+    s1 = _mm256_xor_si256(s1, _mm256_srli_epi64(y, 26));
+      
+    // Let's convert to 8 signed 32-bit floats and return that
+    // This will give us random numbers centered around 0!
+    // We also need to multiply by a prefactor of 2^(-31) to get
+    // the range to be (-1, 1)
+    return _mm256_mul_ps(_mm256_cvtepi32_ps(_mm256_add_epi64(y, s1)), _mm256_set1_ps(4.6566129e-10));
   }
 };
 
-
+// -------------------------------------------------------------------------------------------------
+// Online mask filler class
+// Does good stuff
 struct online_mask_filler : public wi_transform {
-    // Specified at construction.
-    // Note that nt_chunk is a member of the wi_transform base class.
     const int v1_chunk;
     const float var_weight;
     const float var_clamp_add;
@@ -62,8 +55,7 @@ struct online_mask_filler : public wi_transform {
     const float w_cutoff;
     vector<double> running_weights;
     vector<double> running_var;
-  //    std::mt19937 mt_rand;
-    xorshift_plus rand_x;
+    vec_xorshift_plus rand_x;
     
     online_mask_filler(int v1_chunk, float var_weight, float var_clamp_add, float var_clamp_mult, float w_clamp, float w_cutoff, int nt_chunk);
  
@@ -99,9 +91,6 @@ online_mask_filler::online_mask_filler(int v1_chunk_, float var_weight_, float v
 
     this->name = ss.str();
 
-    //std::random_device rd; 
-    //std::mt19937 mt_rand(rd());
-
     rf_assert (nt_chunk % v1_chunk == 0);
     rf_assert (nt_chunk > 0);
     rf_assert (var_weight > 0);
@@ -128,74 +117,86 @@ void online_mask_filler::start_substream(int isubstream, double t0)
 }
 
 
-inline bool get_v1(const float *intensity, const float *weights, double &v1, int v1_chunk)
-{
-    int zerocount = 0;
-    double vsum = 0;
-    double wsum = 0;
-
-    for (int i=0; i < v1_chunk; ++i)
-    {
-        // I assume this is okay for checking whether the weight is 0?
-        if (weights[i] < 1e-7)
-	  ++zerocount;
-        vsum += intensity[i] * intensity[i] * weights[i];
-        wsum += weights[i];
-    }
-
-    // Check whether enough valid values were passed
-    if (zerocount > v1_chunk * 0.75)
-    {  
-	v1 = 0;
-        return false;
-    }
-    v1 = vsum / wsum;
-    return true;
-}
-
-
 void online_mask_filler::process_chunk(double t0, double t1, float *intensity, float *weights, ssize_t stride, float *pp_intensity, float *pp_weights, ssize_t pp_stride)
 {
-    double v1;   // stores temporary v1 estimate before it is put into running_var    
-
-    for (int ichunk=0; ichunk < nt_chunk-1; ichunk += v1_chunk)
+  float running_var[nfreq]{}; // holds the actual running variance for each frequency -- initialized to 0
+  __m256 vw = _mm256_set1_ps(var_weight);
+  vec_xorshift_plus rn;
+  __m256 vsum, tmp_var, tmp1, tmp2, p0, p1, w0, w1, w2, w3, i0, i1, i2, i3, res0, res1, res2, res3;
+  __m256 c = _mm256_set1_ps(w_cutoff);
+  __m256 x; // for the xor at the end
+  __m256 root_three = _mm256_sqrt_ps(_mm256_set1_ps(3)); // handy for later
+  __m256 two = _mm256_set1_ps(2);
+    
+  for (int iter=0; iter<niter; iter++)
     {
-        for (int ifreq=0; ifreq < nfreq; ++ifreq)
-        {
-	    const float *iacc = &intensity[ifreq*stride + ichunk];
-	    const float *wacc = &weights[ifreq*stride + ichunk];
+      for (int ifreq=0; ifreq<nfreq; ifreq++)
+	{
+	  for (int ichunk=0; ichunk<nt_chunk-1; ichunk += 32)
+	    {
+	      // Load everything
+	      i0 = _mm256_load_ps(intensity + ifreq * stride + ichunk);
+	      i1 = _mm256_load_ps(intensity + ifreq * stride + ichunk + 8);
+	      i2 = _mm256_load_ps(intensity + ifreq * stride + ichunk + 16);
+	      i3 = _mm256_load_ps(intensity + ifreq * stride + ichunk + 24);
+	          
+	      // Compute sum of squares
+	      vsum = _mm256_fmadd_ps(i3, i3, _mm256_fmadd_ps(i2, i2, _mm256_fmadd_ps(i1, i1, _mm256_mul_ps(i0, i0))));
+	      p0 = _mm256_add_ps(_mm256_permute2f128_ps(vsum, vsum, 0b00100001), vsum);
+	      p1 = _mm256_add_ps(_mm256_permute_ps(p0, 0b00111001), p0);
+	      tmp_var = _mm256_add_ps(_mm256_permute_ps(p1, 0b01001110), p1);
 
-	    // Get v1_chunk
-	    if (get_v1(iacc, wacc, v1, v1_chunk))
-	    {
-	        // If the v1 was succesful, try to increase the weight, if possible
-	        running_weights[ifreq] = min(2.0, running_weights[ifreq] + w_clamp);
-	        // Then, restrict the change in variance estimate definted by the clamp parameters
-	        v1 = min((1 - var_weight) * running_var[ifreq] + var_weight * v1, running_var[ifreq] + var_clamp_add + running_var[ifreq] * var_clamp_mult);
-	        v1 = max(v1, running_var[ifreq] - var_clamp_add - running_var[ifreq] * var_clamp_mult);
-	        // Finally, update the running variance
-	        running_var[ifreq] = v1;
+	      // Then, use update rules to update running variance
+	      tmp1 = _mm256_fmadd_ps(vw, tmp_var, _mm256_set1_ps(1 - var_weight * running_var[ifreq]));
+	      tmp2 = _mm256_set1_ps(running_var[ifreq] + var_clamp_add + running_var[ifreq] * var_clamp_mult);
+	      tmp_var = _mm256_min_ps(tmp1, tmp2);
+	      tmp_var = _mm256_max_ps(tmp_var, _mm256_set1_ps(running_var[ifreq] - var_clamp_add - running_var[ifreq] * var_clamp_mult));
+	          
+	      // Finally, mask fill with the running variance
+	      w0 = _mm256_load_ps((const float*) weights + ifreq * stride + ichunk);
+	      w1 = _mm256_load_ps((const float*) weights + ifreq * stride + ichunk + 8);
+	      w2 = _mm256_load_ps((const float*) weights + ifreq * stride + ichunk + 16);
+	      w3 = _mm256_load_ps((const float*) weights + ifreq * stride + ichunk + 24);
+	          
+	      // If weights less than cutoff, fill
+	      res0 = _mm256_blendv_ps(i0, _mm256_mul_ps(rn.gen_floats(), root_three), _mm256_cmp_ps(w0, c, _CMP_LT_OS));
+	      res1 = _mm256_blendv_ps(i1, _mm256_mul_ps(rn.gen_floats(), root_three), _mm256_cmp_ps(w1, c, _CMP_LT_OS));
+	      res2 = _mm256_blendv_ps(i2, _mm256_mul_ps(rn.gen_floats(), root_three), _mm256_cmp_ps(w2, c, _CMP_LT_OS));
+	      res3 = _mm256_blendv_ps(i3, _mm256_mul_ps(rn.gen_floats(), root_three), _mm256_cmp_ps(w3, c, _CMP_LT_OS));
+
+	      // Finally, store the new intensity values
+	      _mm256_storeu_ps((float*) (intensity + ifreq * stride + ichunk), res0);
+	      _mm256_storeu_ps((float*) (intensity + ifreq * stride + ichunk + 8), res1);
+	      _mm256_storeu_ps((float*) (intensity + ifreq * stride + ichunk + 16), res2);
+	      _mm256_storeu_ps((float*) (intensity + ifreq * stride + ichunk + 24), res3);
+
+	      // Also, bump all weights up to 2.0 -- there is no "failed" variance estimation option yet
+	      _mm256_storeu_ps((float*) (weights + ifreq * stride + ichunk), two);
+	      _mm256_storeu_ps((float*) (weights + ifreq * stride + ichunk + 8), two);
+	      _mm256_storeu_ps((float*) (weights + ifreq * stride + ichunk + 16), two);
+	      _mm256_storeu_ps((float*) (weights + ifreq * stride + ichunk + 24), two);
+
+	      // Artificial xor and write to force iterations to execute - I'm not quite sure this covers it (e.g. will the stores be forced?)
+	      x = _mm256_xor_ps(_mm256_xor_ps(_mm256_xor_ps(res0, res1), res2), res3);      
 	    }
-	    else
-	    {
-	        // For an unsuccessful v1, we decrease the weight if possible. We do not modify the running variance
-	        running_weights[ifreq] = max(0.0, running_weights[ifreq] - w_clamp);
-	    }
-	      
-	    // Do the mask filling for a particular frequency using our new variance estimate
-	    //std::uniform_real_distribution<double> dist(-sqrt(3*running_var[ifreq]), sqrt(3*running_var[ifreq]));
-	    //std::normal_distribution<double> dist(0, sqrt(running_var[ifreq]));
-	    for (int i=0; i < v1_chunk; ++i)
-	    {
-	        if (running_weights[ifreq] != 0)
-		{
-		    if (weights[ifreq*stride+i+ichunk] < w_cutoff)
-		      intensity[ifreq*stride+i+ichunk] = rand_x.gen_float() * 2 * (sqrt(3 * running_var[ifreq])) - sqrt(3 * running_var[ifreq]);
-		}
-	        weights[ifreq*stride+i+ichunk] = running_weights[ifreq];
-	    }
-	} // close the frequency loop
-    } // close the ichunk loop
+
+	  // Update the (scalar) running variance -- thanks Kendrick!
+	  // Convert the constant register 'x' to a scalar, and write to q[0].
+	  // First step: extract elements 0-3 into a 128-bit register.
+	  __m128 y = _mm256_extractf128_ps(tmp_var, 0);
+	  
+	  // The second step is really strange!  The intrinsic _mm_extract_ps()
+	  // extracts element 0 from the 128-bit register, but it has the wrong
+	  // return type (int32 instead of float32).  The returned value is a "fake"
+	  // int32 obtained by interpreting the bit pattern of the "real" float32
+	  // (in IEEE-754 representation) as an int32 (in twos-complement representation),
+	  // so it's not very useful.  Nevertheless if we just write it to memory as an int32,
+	  // and read back from the same memory location later as a float32, we'll get the
+	  // right answer.  This doesn't make much sense and there must be a better way to do it!
+	  int *qi = reinterpret_cast<int *> (&running_var[ifreq]);   // hack: (int *) pointing to same memory location as q[0]
+	  *qi = _mm_extract_ps(y, 0); // write a "fake" int32 to this memory location
+	}
+    }  
 }
 
 
