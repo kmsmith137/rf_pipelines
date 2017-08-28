1ac8a8b9e33662469a6e5d1e8804f15f5240f275
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-28 17:34:07 -0400

    rf_pipelines::wi_downsample() now uses rf_kernels::wi_downsampler

--- udsample.cpp
+++ udsample.cpp
@@ -5,9 +5,8 @@
 // improve this by having special kernels to handle the large-Df and large-Dt cases.
 
 #include <cassert>
+#include "rf_kernels/downsample.hpp"
 #include "rf_pipelines_internals.hpp"
-
-#include "kernels/downsample.hpp"
 
 using namespace std;
 
@@ -17,147 +16,32 @@ namespace rf_pipelines {
 #endif
 
 
-// Note: kernels/downsample.hpp defines
-//   _kernel_downsample_2d<T,S,Df,Dt> (out_intensity, out_weights, out_stride, in_intensity, in_nfreq, in_nt, in_stride)
-
-using downsampling_kernel_t = void (*) (float *, float *, int, const float *, const float *, int, int, int);
-
-
-// -------------------------------------------------------------------------------------------------
-//
-// fill_2d_downsampling_kernel_table<S,MaxDf,MaxDt>()
-
-
-template<unsigned int S, unsigned int Df, unsigned int MaxDt, typename std::enable_if<(MaxDt==0),int>::type = 0>
-inline void fill_1d_downsampling_kernel_table(downsampling_kernel_t *out) { }
-
-template<unsigned int S, unsigned int Df, unsigned int MaxDt, typename std::enable_if<(MaxDt>0),int>::type = 0>
-inline void fill_1d_downsampling_kernel_table(downsampling_kernel_t *out)
-{
-    constexpr int NDt = IntegerLog2<MaxDt>() + 1;
-
-    fill_1d_downsampling_kernel_table<S,Df,(MaxDt/2)> (out);
-    out[NDt-1] = _kernel_downsample_2d<float,S,Df,MaxDt>;
-}
-
-
-template<unsigned int S, unsigned int MaxDf, unsigned int MaxDt, typename std::enable_if<(MaxDf==0),int>::type = 0>
-inline void fill_2d_downsampling_kernel_table(downsampling_kernel_t *out) { }
-
-template<unsigned int S, unsigned int MaxDf, unsigned int MaxDt, typename std::enable_if<(MaxDf>0),int>::type = 0>
-inline void fill_2d_downsampling_kernel_table(downsampling_kernel_t *out) 
-{ 
-    constexpr int NDf = IntegerLog2<MaxDf>() + 1;
-    constexpr int NDt = IntegerLog2<MaxDt>() + 1;
-
-    fill_2d_downsampling_kernel_table<S,(MaxDf/2),MaxDt> (out);
-    fill_1d_downsampling_kernel_table<S,MaxDf,MaxDt> (out + (NDf-1)*NDt);
-}
-
-
-// -------------------------------------------------------------------------------------------------
-
-
-struct downsampling_kernel_table {
-    static constexpr int S = constants::single_precision_simd_length;
-    static constexpr int MaxDf = constants::max_frequency_downsampling;
-    static constexpr int MaxDt = constants::max_time_downsampling;
-    static constexpr int NDf = IntegerLog2<MaxDf>() + 1;
-    static constexpr int NDt = IntegerLog2<MaxDt>() + 1;
-
-    // Weird: for some reason using std::max() here gives a clang linker (not compiler) error.
-    static constexpr int MaxD = (MaxDf > MaxDt) ? MaxDf : MaxDt;
-
-    std::vector<downsampling_kernel_t> entries;
-
-    integer_log2_lookup_table ilog2_lookup;
-
-
-    downsampling_kernel_table() : entries(NDf*NDt), ilog2_lookup(MaxD)
-    {
-	fill_2d_downsampling_kernel_table<S,MaxDf,MaxDt> (&entries[0]);
-    }
-
-    // Before calling get_kernel(), caller must check:
-    //    1 <= Df <= MaxDf 
-    //    1 <= Dt <= MaxDt
-    //    both Df,Dt are powers of two
-
-    inline downsampling_kernel_t get_kernel(int Df, int Dt)
-    {
-	int idf = ilog2_lookup(Df);
-	int idt = ilog2_lookup(Dt);
-
-	return entries[idf*NDt + idt];
-    }
-};
-
-
-// -------------------------------------------------------------------------------------------------
-
-
-static downsampling_kernel_table global_downsampling_kernel_table;
-
-
 // Externally visible downsampling routine declared in rf_pipelines.hpp
 //
 // Note that the normalization of the downsampled weights array differs (by a factor of Df*Dt) 
 // from the python version of wi_downsample().  This is actually nontrivial to change, since
 // the unit tests currently assume strict equivalence with the downsampling logic in the
 // intensity_clipper, which we don't want to slow down by including an extra multiplication.
-//
+
 void wi_downsample(float *out_intensity, float *out_weights, int out_stride, const float *in_intensity, const float *in_weights, int in_nfreq, int in_nt, int in_stride, int Df, int Dt)
 {
-    static constexpr int S = constants::single_precision_simd_length;
-    static constexpr int MaxDf = constants::max_frequency_downsampling;
-    static constexpr int MaxDt = constants::max_time_downsampling;
-
-    // Part 1: Argument megacheck
-
     if (_unlikely((in_nfreq <= 0) || (in_nt <= 0)))
 	throw runtime_error("wi_downsample(): (in_nfreq,in_nt)=(" + to_string(in_nfreq) + "," + to_string(in_nt) + ") is invalid");
 
     if (_unlikely((Df <= 0) || (Dt <= 0)))
 	throw runtime_error("wi_downsample(): (Df,Dt)=(" + to_string(Df) + "," + to_string(Dt) + ") is invalid");
 
-    if (_unlikely(!is_power_of_two(Df) || !is_power_of_two(Dt)))
-	throw runtime_error("wi_downsample(): (Df,Dt)=(" + to_string(Df) + "," + to_string(Dt) + ") must be powers of two");
-
     if (_unlikely(in_nfreq % Df))
 	throw runtime_error("wi_downsample(): in_nfreq=" + to_string(in_nfreq) + " is not divisible by Df=" + to_string(Df));
 
-    if (_unlikely(in_nt % (Dt*S)))
-	throw runtime_error("wi_downsample(): in_nt=" + to_string(in_nfreq) + " is not divisible by Dt*S, where Dt=" + to_string(Dt)
-			    + " is the time downsampling factor and S=" + to_string(S) + " is the single-precision simd length on this machine");
+    if (_unlikely(in_nt % Dt))
+	throw runtime_error("wi_downsample(): in_nt=" + to_string(in_nfreq) + " is not divisible by Dt=" + to_string(Dt));
 
-    if (_unlikely(out_stride < in_nt/Dt))
-	throw runtime_error("wi_downsample(): out_stride=" + to_string(out_stride) + " is < (in_nt/Dt) =" + to_string(in_nt/Dt));
+    int nfreq_out = in_nfreq / Df;
+    int nt_out = in_nt / Dt;
     
-    if (_unlikely(in_stride < in_nt))
-	throw runtime_error("wi_downsample(): in_stride=" + to_string(in_stride) + " is < in_nt=" + to_string(in_nt));	
-
-    if (_unlikely((Df > MaxDf) || (Dt > MaxDt))) {
-	throw runtime_error("wi_downsample(): (Df,Dt)=(" + to_string(Df) + "," + to_string(Dt) + ")"
-			    + " exceeds compile time limits; to fix this see 'constants' in rf_pipelines.hpp");
-    }
-
-    if (_unlikely(!out_intensity))
-	throw runtime_error("wi_downsample(): 'out_intensity' argument is a NULL pointer");
-
-    if (_unlikely(!out_weights))
-	throw runtime_error("wi_downsample(): 'out_weights' argument is a NULL pointer");
-
-    if (_unlikely(!in_intensity))
-	throw runtime_error("wi_downsample(): 'in_intensity' argument is a NULL pointer");
-
-    if (_unlikely(!in_weights))
-	throw runtime_error("wi_downsample(): 'in_weights' argument is a NULL pointer");
-
-    // Part 2: Get and apply downsampling kernel
-
-    auto kernel = global_downsampling_kernel_table.get_kernel(Df, Dt);
-    
-    kernel(out_intensity, out_weights, out_stride, in_intensity, in_weights, in_nfreq, in_nt, in_stride);
+    rf_kernels::wi_downsampler ds(Df, Dt);
+    ds.downsample(nfreq_out, nt_out, out_intensity, out_weights, out_stride, in_intensity, in_weights, in_stride);
 }
 
 
