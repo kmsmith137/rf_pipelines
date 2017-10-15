51df0b02a6414b9a90dc2c5bdcc5e79acfd86387
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-10-15 09:58:37 -0400

    start test-almost-everything.py (work in progress)

---
+++ test-almost-everything.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import numpy.random as rand
+
+import rf_pipelines
+
+
+def fact2(n, kmin=1):
+    """Factorizes n as (2^m k), where k is odd, and returns m."""
+
+    assert n >= kmin
+
+    m = 0
+    while ((n % 2) == 0) and (n >= 2*kmin):
+        (m, n) = (m+1, n//2)
+
+    return m
+
+
+####################################################################################################
+
+
+class initial_stream(rf_pipelines.wi_stream):
+    def __init__(self):
+        rf_pipelines.wi_stream.__init__(self, 'initial_stream')
+        self.nfreq = 2**rand.randint(0, 10)
+        self.nfreq *= rand.randint(128//self.nfreq + 1, 2048//self.nfreq + 1)
+        self.nt_tot = rand.randint(1000, 2000)
+        self.intensity = rand.standard_normal(size=(self.nfreq,self.nt_tot))
+        self.weights = rand.uniform(0.5, 1.0, size=(self.nfreq,self.nt_tot))
+        self.nt_chunk = rand.randint(20, 50)
+
+
+    def _fill_chunk(self, intensity, weights, pos):
+        intensity[:,:] = 0.
+        weights[:,:] = 0.
+
+        if pos >= self.nt_tot:
+            return False
+
+        n = min(self.nt_tot - pos, self.nt_chunk)
+        intensity[:,:n] = self.intensity_arr[:,pos:(pos+n)]
+        weights[:,:n] = self.weights_arr[:,pos:(pos+n)]
+        return True            
+
+
+class final_transform(rf_pipelines.wi_transform):
+    def __init__(self, nt_chunk=None):
+        if nt_chunk is None:
+            nt_chunk = rand.randint(10,20)
+
+        rf_pipelines.wi_transform.__init__(self, "final_transform")
+        self.nt_chunk = nt_chunk
+        self.intensity_chunks = [ ]
+        self.weight_chunks = [ ]
+
+    def _process_chunk(self, intensity, weights, pos):
+        self.intensity_chunks.append(np.copy(intensity))
+        self.weight_chunks.append(np.copy(weights))
+
+    def get_results(self):
+        intensity = np.concatenate(self.intensity_chunks, axis=1)
+        weights = np.concatenate(self.weight_chunks, axis=1)
+        return (intensity, weights)
+    
+
+####################################################################################################
+#
+# make_random_*
+#
+# These functions return json-seralized random pipeline_objects (not the pipeline_objects themselves)
+
+
+def make_random_polynomial_detrender(nfreq_ds, nds):
+    assert nfreq_ds > 0
+    assert nds > 0
+
+    if (rand.randint(0,2)) and (nfreq_ds >= 16):
+        axis = 'AXIS_FREQ'
+        maxdeg = (nfreq_ds // 8) - 2
+        maxdeg = min(maxdeg, 4)
+        polydeg = rand.randint(0, maxdeg+1)
+        nt_chunk = 8 * max(rand.randint(-5,5), 0)
+    else:
+        axis = 'AXIS_TIME'
+        polydeg = rand.randint(0,5)
+        nt_chunk = 8 * nds * rand.randint(polydeg+2, 2*polydeg+4)
+
+    return {
+        'class_name': 'polynomial_detrender',
+        'nt_chunk': nt_chunk,
+        'axis': axis,
+        'polydeg': polydeg,
+        'epsilon': rand.uniform(1.0e-2, 2.0e-2)
+    }
+
+
+def make_random_spline_detrender(nfreq_ds, nds):
+    assert nfreq_ds >= 32
+    max_nbins = nfreq_ds // 32
+
+    return {
+        'class_name': 'spline_detrender',
+        'nt_chunk': 8 * nds * max(rand.randint(-5,10),0),
+        'axis': 'AXIS_FREQ',
+        'nbins': rand.randint(1, min(max_nbins+1,5)),
+        'epsilon': rand.uniform(3.0e-4, 6.0e-4)
+    }
+
+
+def make_random_intensity_clipper(nfreq_ds, nds):
+    assert nfreq_ds >= 32
+
+    a = rand.randint(0,3)
+    Dt = 2**max(rand.randint(-3,5),0)
+
+    if (a == 0) and (nfreq_ds >= 32):
+        axis = 'AXIS_FREQ'
+        m = fact2(nfreq_ds, kmin=32)
+        Df = 2**max(rand.randint(-3,m+1),0)
+        nt_chunk = 8 * nds * Dt * rand.randint(1, 8)
+    elif a == 1:
+        axis = 'AXIS_TIME'
+        m = fact2(nfreq_ds)
+        Df = 2**max(rand.randint(-3,m+1),0)
+        nt_chunk = 8 * nds * Dt * rand.randint(8, 16)
+    else:
+        axis = 'AXIS_NONE'
+        m = fact2(nfreq_ds)
+        Df = 2**max(rand.randint(-3,m+1),0)
+        p = (8 * Df) // nfreq_ds
+        nt_chunk = 8 * nds * Dt * rand.randint(p+1, 10)
+
+    return {
+        'class_name': 'intensity_clipper',
+        'axis': axis,
+        'Df': Df,
+        'Dt': Dt,
+        'nt_chunk': nt_chunk,
+        'sigma': rand.uniform(1.7, 2.0),
+        'iter_sigma': rand.uniform(1.7, 2.0),
+        'niter': max(rand.randint(-3,4), 1),
+        'two_pass': True if rand.randint(0,2) else False
+    }
+
+
+def make_random_std_dev_clipper(nfreq_ds, nds):
+    assert nfreq_ds >= 32
+    assert nfreq_ds % 8 == 0
+
+    axis = 'AXIS_FREQ' if rand.randint(0,2) else 'AXIS_TIME'
+    m = fact2(nfreq_ds, kmin=4)
+    Df = 2**max(rand.randint(-3,m-2),0)
+    Dt = 2**max(rand.randint(-3,5),0)
+    nt_chunk = 8 * nds * Dt * rand.randint(8,16)
+
+    return {
+        'class_name': 'std_dev_clipper',
+        'axis': axis,
+        'Df': Df,
+        'Dt': Dt,
+        'nt_chunk': nt_chunk,
+        'sigma': rand.uniform(1.7, 2.0),
+        'two_pass': True if rand.randint(0,2) else False
+    }
+
+
+def make_random_transform_chain(nfreq_ds, nds):
+    ret = [ make_random_polynomial_detrender(nfreq_ds, nds),
+            make_random_intensity_clipper(nfreq_ds, nds) ]
+
+    if nfreq_ds >= 32:
+        ret.append(make_random_spline_detrender(nfreq_ds, nds))
+
+    if (nfreq_ds % 8 == 0) and (nfreq_ds >= 32):
+        ret.append(make_random_std_dev_clipper(nfreq_ds, nds))
+
+    return ret
+
+
+####################################################################################################
+
+
+for iter in xrange(100):
+    s = initial_stream()
+    tj = make_random_transform_chain(s.nfreq, 1)
+    t = [ rf_pipelines.pipeline_object.from_json(j) for j in tj ]
+    p = rf_pipelines.pipeline([s] + t)
+    p.bind()
+    tj2 = [ x.jsonize() for x in t ]
+    assert tj == tj2
