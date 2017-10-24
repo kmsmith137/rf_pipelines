22c084fba295b9b8a529b56202e24cb75636d85d
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-10-24 17:12:09 -0400

    rf_pipelines.utils.run_for_web_viewer(), moved here from ch_frb_rfi repo

--- rf_pipelines/utils.py
+++ rf_pipelines/utils.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import numpy as np
 
@@ -424,3 +425,38 @@ class Variance_Estimates():
     def _read_h5(self, fname, dset):
         with h5py.File(fname, 'r') as hf:
             return hf[dset][:]
+
+
+####################################################################################################
+
+
+def run_for_web_viewer(run_name, p):
+    """
+    Runs a pipeline, with output directory chosen appropriately for the web viewer
+    at frb1.physics.mcgill.ca.
+
+    The 'run_name' argument should be a short descriptive string.  The pipeline rundir 
+    will look schematically like "(username)/(run_name)_(time)".
+
+    The 'p' argument should be an rf_pipelines.pipeline_object.
+
+    FIXME: what's the best way to generalize this function so that it makes sense
+    on machines other than frb1.physics.mcgill.ca?
+    """
+    
+    assert isinstance(run_name, basestring)
+
+    # Directory names beginning with underscore are pipeline runs in progress.
+    basename = '%s-%s' % (run_name, time.strftime('%y-%m-%d-%X'))
+    dirname = os.path.join('/data2/web_viewer', os.environ['USER'])
+    temp_dir = os.path.join(dirname, '_' + basename)
+    final_dir = os.path.join(dirname, basename)
+
+    print >>sys.stderr, "creating temporary directory '%s' for running pipeline" % temp_dir
+    os.makedirs(temp_dir)
+
+    p.run(outdir=temp_dir, clobber=False)
+
+    # Pipeline done, remove underscore from directory name.
+    print >>sys.stderr, 'renaming %s -> %s' % (temp_dir, final_dir)
+    os.rename(temp_dir, final_dir)
