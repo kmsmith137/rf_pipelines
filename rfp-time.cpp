07a661fe320aac4cc3089d73c9f40e20c74ce131
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-10-26 20:22:47 -0400

    rearrage rfp-time

--- rfp-time.cpp
+++ rfp-time.cpp
@@ -6,57 +6,10 @@ using namespace std;
 using namespace rf_pipelines;
 
 
-// -------------------------------------------------------------------------------------------------
-
-
-static run_params make_run_params()
-{
-    run_params rp;
-    rp.outdir = "";
-    rp.verbosity = 0;
-
-    return rp;
-}
-
-
-static shared_ptr<pipeline_object> make_pipeline(const vector<Json::Value> &json_v)
-{
-    rf_assert(json_v.size() > 0);
-    
-    if (json_v.size() == 1)
-	return pipeline_object::from_json(json_v[0]);
-
-    auto p = make_shared<pipeline> ();
-    
-    for (size_t i = 0; i < json_v.size(); i++)
-	p->add(pipeline_object::from_json(json_v[i]));
-
-    return p;
-}
-
-
-// -------------------------------------------------------------------------------------------------
-//
-// Note: we use bare pointers and don't worry about memory leaks.
-
-
-static void worker_thread_main(const vector<Json::Value> &json_v, Json::Value *json_output)
-{
-    auto rp = make_run_params();
-    auto p = make_pipeline(json_v);
-
-    p->bind(rp);
-    p->allocate();
-    *json_output = p->run(rp);
-}
-
-
-// -------------------------------------------------------------------------------------------------
-
-
 static void usage(const char *msg = nullptr)
 {
     cerr << "Usage: rfp-time [-rP] [-t NTHREADS] file.json [file2.json file3.json ...]\n"
+	 << "   -t: change number of worker threads (default 1)\n"
 	 << "   -r: enable recursive timing of all transforms in pipeline\n"
 	 << "   -P: don't pin threads to cores (default is to pin threads, this should be done on an otherwise idle machine)\n";
 
@@ -73,14 +26,32 @@ static void usage(const string &msg)
 }
 
 
-int main(int argc, char **argv)
-{
-    bool rflag = false;
-    bool tflag = false;
-    bool Pflag = false;
+// -------------------------------------------------------------------------------------------------
+
+
+struct global_context {
+    bool rflag = false;   // -r: enable recursive timing of all transforms in pipeline
+    bool tflag = false;   // -t: change number of worker threads (default 1)
+    bool Pflag = false;   // -P: don't pin threads to cores (default is to pin threads)
     int nthreads = 1;
 
-    vector<char *> json_filenames;
+    run_params rp;
+
+    int ninputs = 0;
+    vector<string> input_filenames;
+    vector<Json::Value> input_json;
+
+    global_context(int argc, char **argv);
+
+    shared_ptr<pipeline_object> make_pipeline() const;
+};
+
+
+global_context::global_context(int argc, char **argv)
+{
+    this->rp.outdir = "";
+    this->rp.verbosity = 0;
+
     int iarg = 1;
 
     while (iarg < argc) {
@@ -89,7 +60,7 @@ int main(int argc, char **argv)
 	iarg++;
 
 	if (arg[0] != '-')
-	    json_filenames.push_back(arg);
+	    this->input_filenames.push_back(arg);
 	else if (!strcmp(arg, "-t")) {
 	    if (iarg >= argc)
 		usage("couldn't parse [-t NTHREADS] argument");
@@ -105,7 +76,8 @@ int main(int argc, char **argv)
 		usage("very large 'nthreads', presumably unintentional: " + to_string(nthreads));
 	    if (tflag)
 		usage("double [-t NTHREADS] argument specified");
-	    tflag = true;
+
+	    this->tflag = true;
 	}
 	else {
 	    if (arglen == 1)
@@ -113,44 +85,85 @@ int main(int argc, char **argv)
 
 	    for (int j = 1; j < arglen; j++) {
 		if (arg[j] == 'r')
-		    rflag = true;
+		    this->rflag = true;
 		else if (arg[j] == 'P')
-		    Pflag = true;
+		    this->Pflag = true;
 		else
 		    usage("unrecognized flag '-" + string(1,arg[j]) + "'");
 	    }
 	}
     }
 
-    int njson = json_filenames.size();
-    vector<Json::Value> json_v(njson);
+    this->ninputs = input_filenames.size();
+    this->input_json.resize(ninputs);
 	
-    if (njson == 0)
+    if (ninputs == 0)
 	usage();
 
     // Read json files
-    for (int i = 0; i < njson; i++) {
-	std::ifstream f(json_filenames[i]);
+    for (int i = 0; i < ninputs; i++) {
+	std::ifstream f(input_filenames[i]);
 	if (f.fail()) {
-	    cerr << json_filenames[i] << ": couldn't open file\n";
+	    cerr << input_filenames[i] << ": couldn't open file\n";
 	    exit(1);
 	}
-	f >> json_v[i];
+	f >> input_json[i];
     }
+}
+
+
+// Calls bind() but not allocate()
+shared_ptr<pipeline_object> global_context::make_pipeline() const
+{
+    shared_ptr<pipeline_object> ret;
+
+    if (ninputs == 1)
+	ret = pipeline_object::from_json(input_json[0]);
+    else {
+	auto p = make_shared<pipeline> ();
+	for (int i = 0; i < ninputs; i++)
+	    p->add(pipeline_object::from_json(input_json[i]));
+	ret = p;
+    }
+
+    ret->bind(this->rp);
+    return ret;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//
+// Note: we use bare pointers and don't worry about memory leaks.
+
+
+static void worker_thread_main(const global_context &c, Json::Value *json_output)
+{
+    auto p = c.make_pipeline();
+
+    p->allocate();
+    *json_output = p->run(c.rp);
+}
+
+
+// -------------------------------------------------------------------------------------------------
+
+
+
+int main(int argc, char **argv)
+{
+    global_context c(argc, argv);
 
     // Construct and bind throwaway pipeline, so that some error checking
     // happens before spawning worker threads.
-    auto p = make_pipeline(json_v);
-    p->bind(make_run_params());
-    p.reset();
+    c.make_pipeline();
 
     vector<std::thread> threads;
-    vector<Json::Value> json_output(nthreads);
+    vector<Json::Value> json_output(c.nthreads);
 
-    for (int i = 0; i < nthreads; i++)
-	threads.push_back(std::thread(worker_thread_main, json_v, &json_output[i]));
+    for (int i = 0; i < c.nthreads; i++)
+	threads.push_back(std::thread(worker_thread_main, c, &json_output[i]));
 
-    for (int i = 0; i < nthreads; i++)
+    for (int i = 0; i < c.nthreads; i++)
 	threads[i].join();
 
     cout << json_output[0];
