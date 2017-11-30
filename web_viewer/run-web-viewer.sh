e0067dcad52bbfdf735e476e1e87d3f5fb0b2a6e
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-11-30 05:30:45 -0800

    add informational print-statement to run-web-viewer.sh

--- web_viewer/run-web-viewer.sh
+++ web_viewer/run-web-viewer.sh
@@ -3,6 +3,7 @@
 if [[ $# -ne 2 ]]; then
     echo "Usage: run-web-viewer.sh <root_dir> <port>"
     echo "On frb1, you probably want: run-web-viewer.sh /data2/web_viewer 5000"
+    echo "On cf0g9, you probably want: run-web-viewer.sh /frb-archiver-1/web_viewer 5003"
     exit 1
 fi
 
